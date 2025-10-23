c4ea507d0d561ae35d588428929bc2453f0f28dc
hark130 <hark130@yahoo.com>
2025-10-23 09:05:17 -0500

    dev: Initial commit of "Linter" script
    Runs Pycodestyle and then Pylint.
    
    Changes to be committed:
    	new file:   devops/scripts/run_linters.sh

---
+++ devops/scripts/run_linters.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# This script was made to help automate and standardize how Pylint is executed on the code base.
+# This script will execute all commands regardless of how many fail and exit with a non-zero value.
+#
+# This script uses the following exit codes:
+#   0 on success
+#   1 if any Pylint command exits with a non-zero value or an expected directory is missing
+
+
+# GLOBAL VARIABLES
+EXIT_CODE=0                # Value to exit with
+ORIGINAL_DIRECTORY=$(pwd)  # Current working directory
+
+
+# LINTER COMMANDS
+# 1. Pycodestyle
+echo "Running Pycodestyle"
+pycodestyle --max-line-length=100 ./
+if [ $? -ne 0 ]
+then
+    echo "*** Pycodestyle Failed ***"
+    EXIT_CODE=1
+fi
+cd $ORIGINAL_DIRECTORY
+
+# 2. Pylint
+./devops/scripts/run_pylint.sh
+if [ $? -ne 0 ]
+then
+    echo "*** Pylint Failed ***"
+    EXIT_CODE=1
+fi
+cd $ORIGINAL_DIRECTORY
+
+# DONE
+cd $ORIGINAL_DIRECTORY
+if [ $EXIT_CODE -ne 0 ]
+then
+    echo "*** A Linter Failed ***"
+fi
+echo ""
+exit $EXIT_CODE

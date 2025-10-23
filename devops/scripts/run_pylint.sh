7e904225909b41004fbddd5a100fc7e40fe3b895
hark130 <hark130@yahoo.com>
2025-10-23 06:40:58 -0500

    dev: Initial commit of Pylint script
    It appears to be working, but it's failing because Pylint can't find
    files to "lint" (which is fine).
    
    At least it's there for when I get to writing actual code...
    
    Changes to be committed:
    	new file:   devops/scripts/run_pylint.sh

---
+++ devops/scripts/run_pylint.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+# This script was made to help automate and standardize how Pylint is executed on the code base.
+# This script will execute all commands regardless of how many fail and exit with a non-zero value.
+#
+# This script uses the following exit codes:
+#   0 on success
+#   1 if any Pylint command exits with a non-zero value or an expected directory is missing
+
+
+# GLOBAL VARIABLES
+EXIT_CODE=0                # Value to exit with
+ORIGINAL_DIRECTORY=$(pwd)  # Current working directory
+TEST_DIR=test              # Directory containing the project test code
+DEVOPS_DIR=devops          # Directory containing the devops scripts
+# Directories containing the project source code
+SOURCE_DIRS="gallant_input"
+
+
+# PYLINT COMMANDS
+# 1. Source Code
+for SOURCE_DIR in $SOURCE_DIRS
+do
+    cd "$SOURCE_DIR"
+    if [ $? -ne 0 ]
+    then
+        EXIT_CODE=1
+    else
+        echo "Running Pylint on $SOURCE_DIR"
+        # find ./ -type f -name "*.py" -not -name "__init__.py" | xargs python -m pylint --score=no --disable=import-error
+        find ./ -type f -name "*.py" -not -name "__init__.py" | xargs python -m pylint --score=no
+        if [ $? -ne 0 ]
+        then
+            EXIT_CODE=1
+        fi
+    fi
+    cd $ORIGINAL_DIRECTORY
+done
+
+# 2. Test Code
+cd $TEST_DIR
+if [ $? -ne 0 ]
+then
+    EXIT_CODE=1
+else
+    echo "Running Pylint on $TEST_DIR"
+    # find ./ -type f -name "*.py" -not -name "__init__.py" | xargs python -m pylint --score=no --disable=import-error,duplicate-code
+    find ./ -type f -name "*.py" -not -name "__init__.py" | xargs python -m pylint --score=no
+    if [ $? -ne 0 ]
+    then
+        EXIT_CODE=1
+    fi
+fi
+cd $ORIGINAL_DIRECTORY
+
+# 3. Devops Scripts
+cd $DEVOPS_DIR
+if [ $? -ne 0 ]
+then
+    EXIT_CODE=1
+else
+    echo "Running Pylint on $DEVOPS_DIR"
+    # find ./ -type f -name "*.py" -not -name "__init__.py" | xargs python -m pylint --score=no --disable=import-error
+    find ./ -type f -name "*.py" -not -name "__init__.py" | xargs python -m pylint --score=no
+    if [ $? -ne 0 ]
+    then
+        EXIT_CODE=1
+    fi
+fi
+cd $ORIGINAL_DIRECTORY
+
+# DONE
+cd $ORIGINAL_DIRECTORY
+echo ""
+exit $EXIT_CODE

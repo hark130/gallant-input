e1c71de936f36125c1fc5b999ad79c7ba3f1c33c
hark130 <hark130@yahoo.com>
2025-10-24 08:24:44 -0500

    GAIN-2: Added more package constants
    Changes to be committed:
    	modified:   gallant_input/constants.py
    
    Changes not staged for commit:
    	modified:   gallant_input/logger.py
    
    Untracked files:
    	gallant_input/__main__.py
    	gallant_input/arg_parser.py
    	gallant_input/argvals.py
    	gallant_input/main.py

--- gallant_input/constants.py
+++ gallant_input/constants.py
@@ -1,15 +1,41 @@
 """Defines SPOT constants on behalf of the package."""
-# Standard
+
+# Standard Imports
 from typing import Final
-# Third Party
-# Local
+# Third Party Imports
+# Local Imports
 
 
-######################
-# gallant_input.misc #
-######################
+###################
+# gallant_input.* #
+###################
 # Temporary directory constants
 PKG_SHORT_TITLE: Final[str] = 'gain'  # GALLANT INPUT (GAIN)
+# Assumed file extensions
+SIGMF_DATA_FILE_EXT: Final[str] = 'sigmf-data'
+SIGMF_META_FILE_EXT: Final[str] = 'sigmf-meta'
+
+
+############################
+# gallant_input.arg_parser #
+############################
+# Commands
+GAIN_CLI_CMD_ANALYZE: Final[str] = 'analyze'
+GAIN_CLI_CMD_IDENTIFY: Final[str] = 'identify'
+# Command Arguments
+GAIN_CLI_ARG_DATA_FILE: Final[str] = 'datafile'   # .sigmf-data filename
+GAIN_CLI_ARG_META_FILE: Final[str] = 'metafile'   # .sigmf-meta filename
+GAIN_CLI_ARG_SIGMF_BASE: Final[str] = 'basename'  # Base filename for .sigmf-* files
+# General Arguments
+GAIN_CLI_ARG_DEBUG: Final[str] = 'debug'          # Debug logging
+
+
+######################
+# gallant_input.main #
+######################
+EXIT_CODE_SUCCESS: Final[int] = 0  # Success
+EXIT_CODE_INVAL: Final[int] = 1    # Invalid input/environment
+EXIT_CODE_ERROR: Final[int] = 2    # Error encountered during execution
 
 
 ######################

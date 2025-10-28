72f973217b5dfb98d4964b10b1d8738bb61697a8
hark130 <hark130@yahoo.com>
2025-10-28 11:45:09 -0500

    GAIN-3: Made the linters happy
    Left in the TODO though...
    
    Changes to be committed:
    	modified:   gallant_input/sigmfmetaparser.py

--- gallant_input/sigmfmetaparser.py
+++ gallant_input/sigmfmetaparser.py
@@ -35,41 +35,41 @@ class SigMFMetaParser:
 
         Raises:
             FileNotFoundError: The meta data file is not found.
-            KeyError: The object name is invalid or there's a mismatch between object and key.
+            KeyError: The object name is invalid or there's a mismatch between obj_name and key.
             TypeError: Bad data type.
             ValueError: Invalid value.
         """
-        return self.get_object_key_value(object=sigmf.SigMFFile.CAPTURE_KEY,
-                                         key=sigmf.SigMFFile.FREQUENCY_KEY, index=index)
+        return self.get_obj_name_key_value(obj_name=sigmf.SigMFFile.CAPTURE_KEY,
+                                           key=sigmf.SigMFFile.FREQUENCY_KEY, index=index)
 
-    def get_object_key_value(self, object: str, key: str, index: int = 0) -> Any:
+    def get_obj_name_key_value(self, obj_name: str, key: str, index: int = 0) -> Any:
         """Fetch a key value from a SigMF metadata object.
 
         See https://sigmf.org/ for more information on the SigMF metadata objects.
 
         Args:
-            object: The name of the SigMF metadata object to search.  Use sigmf.SigMFFile macros
+            obj_name: The name of the SigMF metadata object to search.  Use sigmf.SigMFFile macros
                 for this argument (e.g., GLOBAL_KEY, CAPTURE_KEY, ANNOTATION_KEY).
                 See sigmf.SigMFFile.VALID_KEYS.keys() for a list of actual values.
-            key: The key to fetch from the specified object.  Use sigmf.SigMFFile macros
+            key: The key to fetch from the specified obj_name.  Use sigmf.SigMFFile macros
                 for this argument.
             index: [OPTIONAL] The index, from the list of captures, to fetch the frequency from.
 
         Returns:
-            The value found at object:key.
+            The value found at obj_name:key.
 
         Raises:
             FileNotFoundError: The meta data file is not found.
             TypeError: The meta_filename argument was not a Path object.
         """
         # LOCAL VARIABLES
-        value = None  # The value from object:key
+        value = None  # The value from obj_name:key
 
         # SETUP
         self.load_data()
 
         # GET IT
-        value = self._get_object_key_value(object=object, key=key, index=index)
+        value = self._get_obj_name_key_value(obj_name=obj_name, key=key, index=index)
 
         # DONE
         return value
@@ -106,36 +106,35 @@ class SigMFMetaParser:
         # DONE
         return base_name
 
-    def _get_object_key_value(self, object: str, key: str, index: int = 0) -> Any:
+    def _get_obj_name_key_value(self, obj_name: str, key: str, index: int = 0) -> Any:
         """Fetch a key value from a SigMF metadata object.
 
         This method does not validate the data has been loaded.  Call self.load_data() first.
 
         Args:
-            object: The name of the SigMF metadata object to search.  Use sigmf.SigMFFile macros
+            obj_name: The name of the SigMF metadata object to search.  Use sigmf.SigMFFile macros
                 for this argument (e.g., GLOBAL_KEY, CAPTURE_KEY, ANNOTATION_KEY).
                 See sigmf.SigMFFile.VALID_KEYS.keys() for a list of actual values.
-            key: The key to fetch from the specified object.  Use sigmf.SigMFFile macros
+            key: The key to fetch from the specified obj_name.  Use sigmf.SigMFFile macros
                 for this argument.
-            index: [Optional] Only used for objects that contain lists (e.g., captures)
+            index: [Optional] Only used for obj_names that contain lists (e.g., captures)
         """
         # LOCAL VARIABLES
-        obj_contents = None  # Object contents from the metadata
-        key_val = None       # Key value from the object dictionary
+        key_val = None       # Key value from the obj_name dictionary
 
         # INPUT VALIDATION
-        # object && key
-        self._validate_object_key_pair(object=object, key=key)
+        # obj_name && key
+        self._validate_obj_name_key_pair(obj_name=obj_name, key=key)
         # index
         validate_type(var=index, var_name='index', var_type=int)
         if index < 0:
             raise TypeError(f'The "index" value is invalid: {index}')
 
         # GET VALUE
-        match object:
+        match obj_name:
             # global
             case sigmf.SigMFFile.GLOBAL_KEY:
-                key_val = self._meta_data.get_global_info(key)
+                key_val = self._meta_data.get_global_field(key)
             # captures
             case sigmf.SigMFFile.CAPTURE_KEY:
                 cap_list = self._meta_data.get_captures()
@@ -144,7 +143,7 @@ class SigMFMetaParser:
             case sigmf.SigMFFile.ANNOTATION_KEY:
                 key_val = self._meta_data.get_annotations(key)
             case _:
-                raise NotImplementedError(f'Object "{object}" passed validation '
+                raise NotImplementedError(f'Object "{obj_name}" passed validation '
                                           'but is not supported')
 
         # DONE
@@ -157,30 +156,30 @@ class SigMFMetaParser:
         """
         self._meta_data = sigmf.sigmffile.fromfile(self._get_base_filename())
 
-    def _validate_object_key_pair(self, object: str, key: str) -> None:
+    def _validate_obj_name_key_pair(self, obj_name: str, key: str) -> None:
         """Validates a SigMF metadata object name against a key.
-        
+
         Raises:
-            KeyError: The object name is invalid or there's a mismatch between object and key.
+            KeyError: The obj_name name is invalid or there's a mismatch between obj_name and key.
         """
         # INPUT VALIDATION
         # Object
-        self._validate_object_name(object=object)
+        self._validate_obj_name_name(obj_name=obj_name)
         # Key
         validate_string(validate_this=key, param_name='key', can_be_empty=False)
 
-    def _validate_object_name(self, object: str) -> None:
+    def _validate_obj_name_name(self, obj_name: str) -> None:
         """Validates a SigMF metadata object name.
 
         Raises:
-            KeyError: The object name is invalid.
+            KeyError: The obj_name name is invalid.
             TypeError: Bad data type.
             ValueError: Invalid value.
         """
         # INPUT VALIDATION
-        validate_string(validate_this=object, param_name='object', can_be_empty=False)
-        if object not in sigmf.SigMFFile.VALID_KEYS.keys():
-            raise KeyError(f'The object name "{object}" is not a valid SigMF key')
+        validate_string(validate_this=obj_name, param_name='obj_name', can_be_empty=False)
+        if obj_name not in sigmf.SigMFFile.VALID_KEYS:
+            raise KeyError(f'The obj_name name "{obj_name}" is not a valid SigMF key')
 
     def _validate_file(self) -> None:
         """Validate the meta filename.

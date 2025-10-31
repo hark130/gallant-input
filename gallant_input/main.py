e7004e5f0a4fba0085cb217a5c9d3869e1510ece
hark130 <hark130@yahoo.com>
2025-10-31 07:48:47 -0500

    GAIN-4: Fully(?) implemented main()
    - Refactored constants usage to make Pylint happy with switch cases
    - Removed DEBUGGING statements
    - Added environment validation
    - Implemented "command" switch case in main()
    
    Manual testing of commands + CLI args is successful.
    The linters are happy.
    This might be done, but it warrants an inspection of the ticket...
    
    Changes to be committed:
    	modified:   gallant_input/main.py

--- gallant_input/main.py
+++ gallant_input/main.py
@@ -1,11 +1,13 @@
 """Entry level function."""
 
 # Standard Imports
+from pathlib import Path
 # Third Party Imports
 # Local Imports
 from gallant_input.arg_parser import parse_args
-from gallant_input.constants import EXIT_CODE_ERROR, EXIT_CODE_INVAL, EXIT_CODE_SUCCESS
 from gallant_input.logger import log_exception, Logger
+from gallant_input.validation import validate_file
+import gallant_input.constants as const
 
 
 # pylint: disable=broad-exception-caught
@@ -19,44 +21,49 @@ def main() -> int:
         All raised exceptions are caught, logged, and then this function immediately returns.
     """
     # LOCAL VARIABLES
-    exit_code = EXIT_CODE_SUCCESS  # Return value
-    argvals = None                 # ArgVals data class w/ parsed args
+    exit_code = const.EXIT_CODE_SUCCESS  # Return value
+    argvals = None                       # ArgVals data class w/ parsed args
 
     # 1. PREPARATION
     try:
-        # INPUT VALIDATION
-
         # PARSE IT
         argvals = parse_args()
-        print(f'COMMAND: {argvals.command}')  # DEBUGGING
-        print(f'DEBUG: {argvals.debug}')  # DEBUGGING
-        print(f'DATA FILE: {argvals.data_file}')  # DEBUGGING
-        print(f'META FILE: {argvals.meta_file}')  # DEBUGGING
 
         # SETUP
         Logger.initialize(debugging=argvals.debug)
 
         # ENVIRONMENT VALIDATION
-        Logger.info('INFO')  # DEBUGGING
-        Logger.debug('DEBUG?')  # DEBUGGING
-        Logger.error('ERROR!')  # DEBUGGING
+        validate_file(validate_this=Path(argvals.data_file),
+                      param_name=f'--{const.GAIN_CLI_ARG_DATA_FILE}', must_exist=True)
+        validate_file(validate_this=Path(argvals.meta_file),
+                      param_name=f'--{const.GAIN_CLI_ARG_META_FILE}', must_exist=True)
     except Exception as err:
         log_exception(error=err)
-        exit_code = EXIT_CODE_INVAL
+        exit_code = const.EXIT_CODE_INVAL
     # 2. EXECUTION
     else:
         try:
             # RUN IT
-            pass  # TODO
+            match argvals.command:
+                # analyze
+                case const.GAIN_CLI_CMD_ANALYZE:
+                    Logger.info('Executing analysis...')
+                # identify
+                case const.GAIN_CLI_CMD_IDENTIFY:
+                    Logger.info('Executing identification...')
+                # ???
+                case _:
+                    raise NotImplementedError(f'Command "{argvals.command}" passed validation '
+                                              'but is not supported')
         except Exception as err:
             log_exception(error=err)
-            exit_code = EXIT_CODE_ERROR
+            exit_code = const.EXIT_CODE_ERROR
 
     # 3. DONE
     try:
         Logger.shutdown()
     except Exception as err:
         log_exception(error=err)
-        if EXIT_CODE_SUCCESS == exit_code:
-            exit_code = EXIT_CODE_ERROR
+        if const.EXIT_CODE_SUCCESS == exit_code:
+            exit_code = const.EXIT_CODE_ERROR
     return exit_code

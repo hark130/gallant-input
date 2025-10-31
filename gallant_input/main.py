3fcdd3133140ecf88872f045045a6fd800752593
hark130 <hark130@yahoo.com>
2025-10-31 08:05:29 -0500

    GAIN-4: Finished main(), for the time being
    - Refactored how filenames are used/stored
    - Analyze command switch case now prints the Center Frequency
    - Added NotImplementedError exception to the identify command
    
    Changes to be committed:
    	modified:   gallant_input/main.py

--- gallant_input/main.py
+++ gallant_input/main.py
@@ -6,6 +6,7 @@ from pathlib import Path
 # Local Imports
 from gallant_input.arg_parser import parse_args
 from gallant_input.logger import log_exception, Logger
+from gallant_input.sigmfmetaparser import SigMFMetaParser
 from gallant_input.validation import validate_file
 import gallant_input.constants as const
 
@@ -23,6 +24,9 @@ def main() -> int:
     # LOCAL VARIABLES
     exit_code = const.EXIT_CODE_SUCCESS  # Return value
     argvals = None                       # ArgVals data class w/ parsed args
+    data_file = None                     # Data filename parsed from CLI args
+    meta_file = None                     # Meta filename parsed from CLI args
+    meta_parser = None                   # SigMFMetaParser object
 
     # 1. PREPARATION
     try:
@@ -33,10 +37,12 @@ def main() -> int:
         Logger.initialize(debugging=argvals.debug)
 
         # ENVIRONMENT VALIDATION
-        validate_file(validate_this=Path(argvals.data_file),
-                      param_name=f'--{const.GAIN_CLI_ARG_DATA_FILE}', must_exist=True)
-        validate_file(validate_this=Path(argvals.meta_file),
-                      param_name=f'--{const.GAIN_CLI_ARG_META_FILE}', must_exist=True)
+        data_file = Path(argvals.data_file)
+        meta_file = Path(argvals.meta_file)
+        validate_file(validate_this=data_file, param_name=f'--{const.GAIN_CLI_ARG_DATA_FILE}',
+                      must_exist=True)
+        validate_file(validate_this=meta_file, param_name=f'--{const.GAIN_CLI_ARG_META_FILE}',
+                      must_exist=True)
     except Exception as err:
         log_exception(error=err)
         exit_code = const.EXIT_CODE_INVAL
@@ -48,9 +54,13 @@ def main() -> int:
                 # analyze
                 case const.GAIN_CLI_CMD_ANALYZE:
                     Logger.info('Executing analysis...')
+                    meta_parser = SigMFMetaParser(meta_filename=meta_file)
+                    print(f'Center Frequency: {meta_parser.get_center_freq()}hz')
                 # identify
                 case const.GAIN_CLI_CMD_IDENTIFY:
                     Logger.info('Executing identification...')
+                    raise NotImplementedError(f'Command "{argvals.command}" is not yet '
+                                              'implemented (see: GAIN-6)')
                 # ???
                 case _:
                     raise NotImplementedError(f'Command "{argvals.command}" passed validation '

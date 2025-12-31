3c76b8787b2f0b863343d566fce6afd09af8cecc
hark130 <hark130@yahoo.com>
2025-12-31 14:28:17 -0600

    GAIN-5: Ordered the GAIN.rds exceptions
    Also, I decided to keep both exceptions (for now).
    Why?
    It seems that RDSBlockIDMismatch is one example of something that would
    cause a RDSIntegrityFailure.
    
    Changes to be committed:
    	modified:   gallant_input/rds/exceptions.py
    
    Changes not staged for commit:
    	modified:   gallant_input/rds/block.py
    	modified:   test/unit_test/test_rds/test_rds_block/test_rds_block_verify_block_integrity.py
    
    Untracked files:
    	rds_poc.py

--- gallant_input/rds/exceptions.py
+++ gallant_input/rds/exceptions.py
@@ -1,11 +1,11 @@
 """Defines bespoke RDS exceptions for use in this sub-package."""
 
 
-class RDSIntegrityFailure(Exception):
-    """An RDS block has failed its integrity check."""
+class RDSBlockIDMismatch(Exception):
+    """An RDS block does not match the expected Block ID."""
     pass
 
 
-class RDSBlockIDMismatch(Exception):
-    """An RDS block does not match the expected Block ID."""
+class RDSIntegrityFailure(Exception):
+    """An RDS block has failed its integrity check."""
     pass

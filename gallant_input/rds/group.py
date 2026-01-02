3d5869286d5606ea9c955dd5428f4a2d859e1cf6
hark130 <hark130@yahoo.com>
2026-01-02 17:44:29 -0600

    GAIN-5: Initial commit of RDSGroup class
    This new class takes in a bytes object, validates its length (104),
    parses it down to block-size chunks, and utilizes RDSBlock to validate
    each and answer questions.
    
    Utilizes the RDSGroupInfo data class to parse the information found in
    the RDS group so the caller and query key values.
    
    No test code yet.  Only manual usage.  I tried to manually use the
    RDSBlock unit test code input only to discover I needed a valid sequence
    of related blocks.  Good news is, 'ole rds_poc.py showed me some
    sequential RDS blocks to use.  (see: python rds_poc.py | grep passed
    and look at 73-76)  It is statistically improbable that these were
    decoded improperly from the source (see: 5.03 RDS.sigmf), even knowing
    that only ~33% of the blocks are valid, given the RDS specification's
    builtin integriy design.  That means, this *must* be working... even
    if the results don't make any sense.  (E.g., Why would a PI code
    assigned to 88.10 FM be anywhere near 99.5?!)  I just have to guess
    that this input is fabricated for the purposes of the exercise because
    there's no pilot tone, music, etc.  That, or maybe the RDS is old...
    the metadata says it's from more than 2 years ago.  Maybe the station
    changed hands.  Who know?
    
    The true test requires an actual flowgraph reading RDS from an FM
    station, pass that data through the code, and compare the results to
    objective research (Google search, Radio faceplate, etc).
    
    Meanwhile, it's time to write some test code and finish up RDSGroup.
    Then, the linters.
    Then, move up the call chain to RDSPICode()
    
    EOD Commit
    
    Changes to be committed:
    	new file:   gallant_input/rds/group.py
    
    Untracked files:
    	rds_poc.py

---
+++ gallant_input/rds/group.py
@@ -0,0 +1,146 @@
+"""Defines a class to parse a Radio Data System (RDS) group of blocks."""
+
+# Standard Imports
+from typing import Final
+# Third Party Imports
+# Local Imports
+from gallant_input.converters import convert_bin_bytes_to_int
+from gallant_input.rds.block import RDSBlock
+from gallant_input.rds.block_id import BlockID
+from gallant_input.rds.group_info import RDSGroupInfo
+from gallant_input.rds.constants import RDS_BLOCK_DATA_LEN, RDS_BLOCK_LEN, RDS_GROUP_LEN
+from gallant_input.rds.exceptions import RDSBlockIDMismatch, RDSIntegrityFailure
+from gallant_input.validation import validate_bytes, validate_type
+
+
+class RDSGroup:
+    """Parse a group of Radio Data System (RDS) blocks."""
+
+    # CORE CLASS METHODS
+    # Methods listed in call order
+    def __init__(self, rds_group: bytes, assume_na:bool = True) -> None:
+        """RDSBlock ctor.
+
+        TO DO: DON'T DO NOW... programmatically determine the region, North America or otherwise,
+        based on the PI code.
+
+        Args:
+            rds_group: The CRC calculated from the RDS block data segment.
+            assume_na: Assume the region is North America (e.g., RBDS vs RDS PTY codes).        
+        """
+        self._murica = assume_na     # MURICA!
+        self._rds_group = rds_group  # The RDS block
+        self._rds_block_a = None     # The RDS group's RDSBlock Block A object
+        self._rds_block_b = None     # The RDS group's RDSBlock Block B object
+        self._rds_block_c = None     # The RDS group's RDSBlock Block C or C' object
+        self._rds_block_d = None     # The RDS group's RDSBlock Block D object
+        self._validated = False      # Validate the internals once
+        self._verified = False       # Verify the group integrity once
+        self._group_info = None      # RDSGroupInfo dataclass object
+
+    # COMMON-USE METHODS
+    # Methods listed in alphabetical order
+
+    def verify_group_integrity(self) -> None:
+        """Validate the RDS group provided.
+
+        Always call this method first when defining public methods.
+
+        1. Validates internals
+        2. Validate block IDs: A, B, C*, D
+        3. Parse group information
+
+        Raises:
+            RDSIntegrityFailure: The RDS block has failed its integrity check.
+            TypeError: Invalid data type.
+            ValueError: Invalid value.
+        """
+        if self._verified is False:
+            # VALIDATION
+            self._validate_internals()
+
+            # PREPARE
+            self._split_rds_group()  # Split the group into four blocks
+
+            # VERIFY INTEGRITY
+            # Validate block IDs
+            try:
+                self._validate_rds_group_integrity()
+            except RDSBlockIDMismatch as err:
+                raise RDSIntegrityFailure('This RDS group failed its integrity check: '
+                                          f'{err}') from err
+            # Parse group information
+            self._parse_group_data()
+
+    # CLASS HELPER METHODS
+    # Methods listed in alphabetical order
+
+    def _parse_group_data(self) -> None:
+        """Parse the data from the group into an internal data class."""
+        # LOCAL VARIABLES
+        block_a_data = self._rds_block_a.get_block_data()  # Block A's data section
+        block_b_data = self._rds_block_b.get_block_data()  # Block B's data section
+
+        # PARSE IT
+        self._group_info = \
+        RDSGroupInfo(
+            # BLOCK A
+            pic = block_a_data[:RDS_BLOCK_DATA_LEN],
+            # BLOCK B
+            gtype = block_b_data[:4],
+            msg_ver = block_b_data[4:5],
+            tp = block_b_data[5:6],
+            pty = block_b_data[6:11],
+            dep = block_b_data[11:RDS_BLOCK_DATA_LEN]
+        )
+
+    def _split_rds_group(self) -> None:
+        """Split self._rds_group into its data blocks."""
+        # LOCAL VARIABLES
+        print(f'RDS GROUP: {self._rds_group}')  # DEBUGGING
+        print(f'SLICING FROM {RDS_GROUP_LEN-(4*RDS_BLOCK_LEN)} TO {RDS_GROUP_LEN-(3*RDS_BLOCK_LEN)}')  # DEBUGGING
+        rds_block_a = self._rds_group[RDS_GROUP_LEN-(4*RDS_BLOCK_LEN):
+                                      RDS_GROUP_LEN-(3*RDS_BLOCK_LEN)]
+        rds_block_b = self._rds_group[RDS_GROUP_LEN-(3*RDS_BLOCK_LEN):
+                                      RDS_GROUP_LEN-(2*RDS_BLOCK_LEN)]
+        rds_block_c = self._rds_group[RDS_GROUP_LEN-(2*RDS_BLOCK_LEN):
+                                      RDS_GROUP_LEN-(1*RDS_BLOCK_LEN)]
+        rds_block_d = self._rds_group[RDS_GROUP_LEN-(1*RDS_BLOCK_LEN):
+                                      RDS_GROUP_LEN-(0*RDS_BLOCK_LEN)]
+
+        # SPLIT IT
+        self._rds_block_a = RDSBlock(rds_block=rds_block_a, block_id=BlockID.BLOCK_A)
+        self._rds_block_b = RDSBlock(rds_block=rds_block_b, block_id=BlockID.BLOCK_B)
+        self._rds_block_c = RDSBlock(rds_block=rds_block_c, block_id=BlockID.BLOCK_C_OR_CP)
+        self._rds_block_d = RDSBlock(rds_block=rds_block_d, block_id=BlockID.BLOCK_D)
+
+    def _validate_internals(self) -> None:
+        """Validate the private attributes once."""
+        if self._validated is False:
+            # self._validated
+            validate_type(var=self._validated, var_name='_validated attribute', var_type=bool)
+            # self._murica
+            validate_type(var=self._murica, var_name='assume_na', var_type=bool)
+            # self._rds_group
+            self._validate_rds_group()
+            self._validated = True
+
+    def _validate_rds_group(self) -> None:
+        """Validate the ctor's arg on behalf of the class."""
+        validate_bytes(validate_this=self._rds_group, param_name='rds_group',
+                       exact_len=RDS_GROUP_LEN)
+        # RDS group content will be validated by the respective RDSBlock objects
+
+    def _validate_rds_group_integrity(self) -> None:
+        """Validate each of the RDS group blocks in turn.
+
+        Raises:
+            RDSIntegrityFailure: The RDS block has failed its integrity check.
+            TypeError: Invalid data type.
+            ValueError: Invalid value.
+        """
+        print(f'BLOCK A: {self._rds_block_a}')  # DEBUGGING
+        self._rds_block_a.verify_block_integrity()
+        self._rds_block_b.verify_block_integrity()
+        self._rds_block_c.verify_block_integrity()
+        self._rds_block_d.verify_block_integrity()

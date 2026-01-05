c848606101b17a600ed34991479c8f48e97f762c
hark130 <hark130@yahoo.com>
2026-01-05 06:31:25 -0600

    GAIN-5: Moved RDS block test case input up a level
    Moved all the TestRDSBlock RDS block test input attributes up a level
    (so I can use them in TestRDSGroup).  Also, updated the inheritance to
    actually inherit from RDSUnitTest (which wasn't I already?).
    
    Changes to be committed:
    	modified:   test/unit_test/test_rds/test_rds.py
    	modified:   test/unit_test/test_rds/test_rds_block/test_rds_block.py
    
    Changes not staged for commit:
    	modified:   gallant_input/rds/group.py
    	modified:   test/unit_test/test_rds/test_rds_block/__main__.py
    	modified:   test/unit_test/test_rds/test_rds_block/test_rds_block_get_block_data.py
    	modified:   test/unit_test/test_rds/test_rds_block/test_rds_block_get_block_id.py
    	modified:   test/unit_test/test_rds/test_rds_block/test_rds_block_verify_block_integrity.py
    
    Untracked files:
    	rds_poc.py
    	test/unit_test/test_rds/test_rds_group/

--- test/unit_test/test_rds/test_rds.py
+++ test/unit_test/test_rds/test_rds.py
@@ -52,6 +52,31 @@ class RDSUnitTest(RootUnitTest):
         test_output_dir:    # Default output directory (OPTIONAL)
     """
 
+    # KNOWN GOOD BLOCK VALUES
+    GOOD_BLOCK_A1 = bytes('01010111000111010101011100', 'utf-8')        # RF JQR 5.03 RDS output
+    GOOD_BLOCK_B1 = bytes('00100001001001011011001000', 'utf-8')        # RF JQR 5.03 RDS output
+    GOOD_BLOCK_C1 = bytes('11001101110011011010110011', 'utf-8')        # RF JQR 5.03 RDS output
+    GOOD_BLOCK_C_PRIME1 = bytes('', 'utf-8')                            # TD: DDN... Find an example
+    GOOD_BLOCK_D1 = bytes('01000110010011010001001011', 'utf-8')        # RF JQR 5.03 RDS output
+    GOOD_BLOCK_E1 = bytes('', 'utf-8')                                  # TD: DDN... Find an example
+    GOOD_BLOCK_A2 = bytes('11001100110011001101110111', 'utf-8')        # External example
+    GOOD_BLOCK_B2 = bytes('00010010001101000010101110', 'utf-8')        # External example
+    GOOD_BLOCK_C2 = bytes('10101010101010100110100100', 'utf-8')        # External example
+    GOOD_BLOCK_C_PRIME2 = bytes('11000011110000110110000001', 'utf-8')  # External example
+    GOOD_BLOCK_D2 = bytes('01100110011001100111001101', 'utf-8')        # External example
+    GOOD_BLOCK_E2 = bytes('11110000111100001001100110', 'utf-8')        # External example
+
+    # BAD BLOCK VALUES
+    BAD_BLOCK1 = None  # TypeError: None
+    BAD_BLOCK2 = tuple(('00100001001001011011001000', 'utf-8'))  # TypeError: tuple
+    BAD_BLOCK3 = 0x15C755C                                       # ValueError: integer value
+    BAD_BLOCK4 = bytes('', 'utf-8')                              # ValueError: Empty
+    BAD_BLOCK5 = bytes('1000110010011010001001011', 'utf-8')     # ValueError: too short
+    BAD_BLOCK6 = bytes('001000110010011010001001011', 'utf-8')   # ValueError: too long
+    # ValueError: two blocks
+    BAD_BLOCK7 = bytes('0101011100011101010101110000100001001001011011001000', 'utf-8')
+    BAD_BLOCK8 = bytes('01000110010021010001001011', 'utf-8')    # ValueError: "I thought I saw a 2"
+
     # CORE CLASS METHODS
     # Methods listed in call order
 

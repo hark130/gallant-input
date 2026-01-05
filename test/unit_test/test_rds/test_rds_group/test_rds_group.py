52f31c926889cc5c32928406ce6abbda794681ca
hark130 <hark130@yahoo.com>
2026-01-05 08:21:27 -0600

    GAIN-5: Initial commit of RDSGroup tests/framework
    TestRDSGroup framework
    17 RDSGroup.verify_group_integrity() unit tests
    
    SPOILERS - All test cases are passing locally
    
    Changes to be committed:
    	new file:   test/unit_test/test_rds/test_rds_group/__init__.py
    	new file:   test/unit_test/test_rds/test_rds_group/__main__.py
    	new file:   test/unit_test/test_rds/test_rds_group/test_rds_group.py
    	new file:   test/unit_test/test_rds/test_rds_group/test_rds_group_verify_group_integrity.py
    
    Changes not staged for commit:
    	modified:   gallant_input/rds/block.py
    	modified:   gallant_input/rds/group.py
    	modified:   test/unit_test/test_rds/test_rds.py
    
    Untracked files:
    	rds_poc.py

---
+++ test/unit_test/test_rds/test_rds_group/test_rds_group.py
@@ -0,0 +1,106 @@
+"""Defines the root RDSGroup unit test class.
+
+RDSGroupUnitTest is the parent class for all rds.group RDSGroup unit test classes.
+
+    Typical usage example:
+
+    from my_module_to_test import my_function_to_test as my_function
+    from test.unit_test.test_rds.test_rds_group.test_rds_group import RDSGroupUnitTest
+
+    class RDSSomethingUnitTest(RDSGroupUnitTest):
+        # Establish the local test (input/output) dirs
+        def __init__(self, *args, **kwargs) -> None:
+        # Default directory for input files
+        self.test_input_dir = os.path.join(HERE, 'test_input')
+        # Default directory for output files
+        self.test_output_dir = os.path.join(HERE, 'test_output')
+
+        # Child class must override this method
+        def call_callable(self):
+            return my_function(*self._args, **self._kwargs)
+
+        # Child class must override this method
+        def validate_return_value(self):
+            self._validate_return_value(return_value=return_value)
+
+        # This is your test case
+        def test_stuff(self):
+            self.set_test_input(1, 2)
+            self.expect_return(3)
+"""
+
+# Standard Imports
+from typing import Any
+# Third Party Imports
+from test.unit_test.test_rds.test_rds import RDSUnitTest
+# Local Imports
+
+
+class RDSGroupUnitTest(RDSUnitTest):
+    """Parent class for all RDSGroup method unit tests.
+
+    Inherit from this class, define necessary functionality for the function you're testing and
+    be sure to override the following methods in your child class:
+        call_callable()
+        validate_return_value()
+
+    Available features:
+        See: help(TediousUnitTest)
+
+    Attributes:
+        test_case_data:   # Stores data about the test case as a TestCaseData object
+        test_input_dir:   # Default input directory (OPTIONAL)
+        test_output_dir:  # Default output directory (OPTIONAL)
+        input_rds_group:  # Test case input: RDSGroup(rds_group)
+        input_assume_na:  # Test case input: RDSGroup(assume_na)
+    """
+
+    # CORE CLASS METHODS
+    # Methods listed in call order
+    def __init__(self, *args, **kwargs) -> None:
+        """RootUnitTest ctor."""
+        # ATTRIBUTES
+        self.input_rds_group = None  # Test case input: RDSGroup(rds_group)
+        self.input_assume_na = None   # Test case input: RDSGroup(assume_na)
+
+        super().__init__(*args, **kwargs)
+
+    def call_callable(self):
+        """Defines how the class will invoke the function call.
+
+        Child class must override this method.  See TediousUnitTest.call_callable() for details.
+        """
+        # Example Usage:
+        # test_obj = RDSGroup(rds_group=self.input_rds_group, assume_na=self.input_assume_na)
+        # return test_obj.the_method_you_are_testing(*self._args, **self._kwargs)
+        raise NotImplementedError(
+            self._test_error.format('The child class must override the call_callable method with '
+                                    'the function to test.'))
+
+    def validate_return_value(self, return_value):
+        """Defines how the class will validate the return value of the tested call.
+
+        Child class must override this method.
+        See TediousUnitTest.validate_return_value() for details.
+        """
+        # Example Usage:
+        # self._validate_return_value(return_value=return_value)
+        raise NotImplementedError(
+            self._test_error.format('The child class must override the validate_return_value '
+                                    'method with the appropriate validation logic'))
+
+    # COMMON-USE METHODS
+    # Methods listed in alphabetical order
+
+    def set_ctor_args(self, rds_group: Any, assume_na: Any) -> None:
+        """Set the class ctor arguments.
+
+        Args:
+            rds_group: Sets the rds_group argument input.  Accepts any input, including bad input.
+            assume_na: Sets the assume_na argument input.  Accepts any input, including bad input.
+        """
+        self.input_rds_group = rds_group
+        self.input_assume_na = assume_na
+
+    # CLASS HELPER METHODS
+    # Methods listed in alphabetical order
---
+++ test/unit_test/test_rds/test_rds_group/test_rds_group_verify_group_integrity.py
@@ -0,0 +1,215 @@
+"""Unit test module for RDSGroup.verify_group_integrity().
+
+Typical Usage:
+    python -m test                                           # Run *all* the test cases
+    python -m test.unit_test                                 # Run *all* the unit test cases
+    python -m test.unit_test.test_rds                        # Run *all* rds sub-package test cases
+    python -m test.unit_test.test_rds.test_rds_group         # Run *all* RDSGroup method test cases
+    # Run just these unit tests
+    python -m test.unit_test.test_rds.test_rds_group.test_rds_group_verify_group_integrity
+    # Run just this normal 1 unit test
+    python -m test.unit_test.test_rds.test_rds_group.test_rds_group_verify_group_integrity -k n01
+"""
+
+# Standard Imports
+from typing import Any
+from unittest import skip
+# Third Party Imports
+from tediousstart.tediousstart import execute_test_cases
+# Local Imports
+from gallant_input.rds.constants import RDS_GROUP_LEN
+from gallant_input.rds.group import RDSGroup
+from gallant_input.rds.exceptions import RDSIntegrityFailure
+from test.unit_test.test_rds.test_rds_group.test_rds_group import RDSGroupUnitTest
+
+
+class RDSGroupVGIUnitTest(RDSGroupUnitTest):
+    """Parent class for all RDSGroup.verify_group_integrity() unit tests."""
+
+    # CORE CLASS METHODS
+    # Methods listed in call order
+
+    def call_callable(self):
+        """Defines how the class will invoke the function call.
+
+        Child class must override this method.  See TediousUnitTest.call_callable() for details.
+        """
+        test_obj = RDSGroup(rds_group=self.input_rds_group, assume_na=self.input_assume_na)
+        return test_obj.verify_group_integrity(*self._args, **self._kwargs)
+
+    def validate_return_value(self, return_value):
+        """Defines how the class will validate the return value of the tested call.
+
+        Child class must override this method.
+        See TediousUnitTest.validate_return_value() for details.
+        """
+        self._validate_return_value(return_value=return_value)
+
+    # COMMON-USE METHODS
+    # Methods listed in alphabetical order
+
+    # CLASS HELPER METHODS
+    # Methods listed in alphabetical order
+
+    def run_test_exception(self, rds_group: Any, assume_na: Any,
+                           exception_type: Exception, exception_msg: str) -> None:
+        """Common method calls for a test case expected to raise an exception.
+
+        Args:
+            rds_group: Sets the rds_group argument input.  Accepts any input, including bad input.
+            assume_na: Sets the assume_na argument input.  Accepts any input, including bad input.
+            exception_type: An Exception type to expect (e.g., ValueError).
+            exception_msg: A sub-string, empty or not, to look for in the raised Exception.
+        """
+        self.set_ctor_args(rds_group=rds_group, assume_na=assume_na)
+        self.set_test_input()  # This method does not take any args
+        self.expect_exception(exception_type=exception_type, exception_msg=exception_msg)
+        self.run_test()
+
+    def run_test_return(self, rds_group: bytes, assume_na: bool) -> None:
+        """Common method calls for a test case expected to return.
+
+        Args:
+            rds_group: Sets the rds_group argument input.
+            assume_na: Sets the assume_na argument input.
+        """
+        self.set_ctor_args(rds_group=rds_group, assume_na=assume_na)
+        self.set_test_input()  # This method does not take any args
+        self.expect_return(None)
+        self.run_test()
+
+
+class NormalRDSGroupVGIUnitTest(RDSGroupVGIUnitTest):
+    """Normal Test Cases."""
+
+    def test_n01_valid_coherent_group1(self):
+        """Valid example of a coherent RDS Group."""
+        rds_group = self.GOOD_GROUP1
+        assume_na = True
+        self.run_test_return(rds_group, assume_na)
+
+    def test_n02_valid_coherent_group1_different_continent(self):
+        """Valid example of a coherent RDS Group but assume it's not from North America."""
+        rds_group = self.GOOD_GROUP1
+        assume_na = False
+        self.run_test_return(rds_group, assume_na)
+
+
+class ErrorRDSGroupVGIUnitTest(RDSGroupVGIUnitTest):
+    """Error Test Cases."""
+
+    def test_e01_rds_group_bad_type_none(self):
+        """Bad rds_group type: None."""
+        rds_group = self.BAD_BLOCK1
+        assume_na = True
+        self.run_test_exception(rds_group, assume_na, TypeError,
+                                'argument should have been of type')
+
+    def test_e02_rds_group_bad_type_tuple(self):
+        """Bad rds_group type: tuple."""
+        rds_group = tuple((self.GOOD_BLOCK_A3, self.GOOD_BLOCK_B3,
+                           self.GOOD_BLOCK_C3, self.GOOD_BLOCK_D3))
+        assume_na = True
+        self.run_test_exception(rds_group, assume_na, TypeError,
+                                'argument should have been of type')
+
+    def test_e03_rds_group_bad_type_int(self):
+        """Bad rds_group type: int."""
+        rds_group = self.BAD_BLOCK3*4
+        assume_na = True
+        self.run_test_exception(rds_group, assume_na, TypeError,
+                                'argument should have been of type')
+
+    def test_e04_rds_group_bad_value_empty(self):
+        """Bad rds_group value: empty."""
+        rds_group = self.BAD_BLOCK4
+        assume_na = True
+        self.run_test_exception(rds_group, assume_na, ValueError, 'The "rds_group" argument must '
+                                'be of length "104" instead of')
+
+    def test_e05_rds_group_bad_value_short(self):
+        """Bad rds_group value: short."""
+        rds_group = self.GOOD_GROUP1[:RDS_GROUP_LEN-1]
+        assume_na = True
+        self.run_test_exception(rds_group, assume_na, ValueError, 'The "rds_group" argument must '
+                                'be of length "104" instead of')
+
+    def test_e06_rds_group_bad_value_long(self):
+        """Bad rds_group value: long."""
+        rds_group = self.GOOD_GROUP1 + b'0'
+        assume_na = True
+        self.run_test_exception(rds_group, assume_na, ValueError, 'The "rds_group" argument must '
+                                'be of length "104" instead of')
+
+    def test_e07_rds_group_bad_value_two_blocks(self):
+        """Bad rds_group value: two groups."""
+        rds_group = self.GOOD_GROUP1 + self.GOOD_GROUP1
+        assume_na = True
+        self.run_test_exception(rds_group, assume_na, ValueError, 'The "rds_group" argument must '
+                                'be of length "104" instead of')
+
+    def test_e08_rds_group_bad_value_binary(self):
+        """Bad rds_group value: binary contains an invalid character."""
+        rds_group = self.GOOD_BLOCK_A3 + self.GOOD_BLOCK_B3 + self.GOOD_BLOCK_C3 + self.BAD_BLOCK8
+        assume_na = True
+        self.run_test_exception(rds_group, assume_na, ValueError, 'Invalid binary value detected')
+
+    def test_e09_assume_na_bad_type_none(self):
+        """Bad assume_na type: None."""
+        rds_group = self.GOOD_GROUP1
+        assume_na = None
+        self.run_test_exception(rds_group, assume_na, TypeError,
+                                'argument should have been of type')
+
+    def test_e10_assume_na_bad_type_tuple(self):
+        """Bad assume_na type: tuple."""
+        rds_group = self.GOOD_GROUP1
+        assume_na = tuple((self.GOOD_GROUP1, True))
+        self.run_test_exception(rds_group, assume_na, TypeError,
+                                'argument should have been of type')
+
+    def test_e11_assume_na_bad_type_int(self):
+        """Bad assume_na type: int."""
+        rds_group = self.GOOD_GROUP1
+        assume_na = 1  # True value
+        self.run_test_exception(rds_group, assume_na, TypeError,
+                                'argument should have been of type')
+
+
+class SpecialRDSGroupGBIUnitTest(RDSGroupVGIUnitTest):
+    """Special Test Cases."""
+
+    def test_s01_out_of_order_group_shift1(self):
+        """A group of RDS blocks that are valid but out of order: caesar shift 1."""
+        rds_group = self.GOOD_BLOCK_D3 + self.GOOD_BLOCK_A3 + self.GOOD_BLOCK_B3 \
+                    + self.GOOD_BLOCK_C3
+        assume_na = True
+        self.run_test_exception(rds_group, assume_na, RDSIntegrityFailure,
+                                'This RDS group failed its integrity check:')
+
+    def test_s02_out_of_order_group_shift2(self):
+        """A group of RDS blocks that are valid but out of order: caesar shift 1."""
+        rds_group = self.GOOD_BLOCK_C3 + self.GOOD_BLOCK_D3 + self.GOOD_BLOCK_A3 \
+                    + self.GOOD_BLOCK_B3 
+        assume_na = True
+        self.run_test_exception(rds_group, assume_na, RDSIntegrityFailure,
+                                'This RDS group failed its integrity check:')
+
+    def test_s03_out_of_order_group_shift3(self):
+        """A group of RDS blocks that are valid but out of order: caesar shift 1."""
+        rds_group = self.GOOD_BLOCK_B3 + self.GOOD_BLOCK_C3 + self.GOOD_BLOCK_D3 \
+                    + self.GOOD_BLOCK_A3
+        assume_na = True
+        self.run_test_exception(rds_group, assume_na, RDSIntegrityFailure,
+                                'This RDS group failed its integrity check:')
+
+    def test_s04_valid_yet_disparate_blocks(self):
+        """A group of RDS blocks that are not necessarily related to each other."""
+        rds_group = self.GOOD_BLOCK_A1 + self.GOOD_BLOCK_B3 + self.GOOD_BLOCK_C1 \
+                    + self.GOOD_BLOCK_D3
+        assume_na = True
+        self.run_test_return(rds_group, assume_na)
+
+
+if __name__ == '__main__':
+    execute_test_cases()

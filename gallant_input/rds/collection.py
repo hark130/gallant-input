adb370cdb3fde01599b6239de527da5b443cb918
hark130 <hark130@yahoo.com>
2026-01-06 14:01:14 -0600

    GAIN-5: Initial commit of RDSCollection class
    Manual test code shows it to be working.
    I won't write test cases for it (yet) because I only have the binary for
    one fully formed and valid RDS group.  Even then, I may not add test
    cases once I have more.  This class is basically a wrapper around a
    wrapper (RDSPICode) around a wrapper (RDSBlock) around a wrapper
    (RDSBlock) and the other layers all have several passing test cases.
    
    At this point, it's time to make the linters happy and then write a test
    harness to read a binary file and then run the bytes through the
    collection, and then query the results (or something along those lines).
    
    Changes to be committed:
    	new file:   gallant_input/rds/collection.py

---
+++ gallant_input/rds/collection.py
@@ -0,0 +1,155 @@
+"""Collect Radio Data System (RDS) groups by Program identification codes (PI code)."""
+
+# Standard Imports
+# Third Party Imports
+# Local Imports
+from gallant_input.rds.constants import RDS_BLOCK_DATA_LEN
+from gallant_input.rds.exceptions import RDSIntegrityFailure, RDSPICodeMismatch
+from gallant_input.rds.group import RDSGroup
+from gallant_input.rds.picode import RDSPICode
+from gallant_input.validation import validate_binary_bytes, validate_list, validate_type
+
+
+class RDSCollection:
+    """Store Radio Data System (RDS) groups by Program Identification code (PI code)."""
+
+    # CORE CLASS METHODS
+    # Methods listed in call order
+
+    def __init__(self) -> None:
+        """RDSCollection ctor."""
+        self._picode_dict = {}   # bytes(pi_code) : RDSPICode
+        self._validated = False  # Validate the internals once
+
+    # COMMON-USE METHODS
+    # Methods listed in alphabetical order
+
+    def add_bytes(self, group_bytes: bytes) -> None:
+        """Add RDS group bytes to the appropriate set.
+
+        Args:
+            group_bytes: A collection of bytes, of length RDS_GROUP_LEN, associated with the
+                previously provided pi_code.
+
+        Raises:
+            RDSIntegrityFailure: The RDS block bytes provided has failed the integrity check.
+            RDSPICodeMismatch: An internal PI code mismatch was detected.
+            TypeError: Invalid data type.
+            ValueError: Invalid value.
+        """
+        rds_group = RDSGroup(rds_group=group_bytes)
+        self._add_rds_group(rds_group=rds_group, var_name='group_bytes')
+
+    def add_rds_group(self, rds_group: RDSGroup) -> None:
+        """Add an RDSGroup object to the appropriate set.
+
+        Args:
+            rds_group: An RDSGroup object associated with the established PI code.
+
+        Raises:
+            RDSIntegrityFailure: The RDS block bytes provided has failed the integrity check.
+            RDSPICodeMismatch: An internal PI code mismatch was detected.
+            TypeError: Invalid data type.
+            ValueError: Invalid value.
+        """
+        self._add_rds_group(rds_group=rds_group, var_name='rds_group')
+
+    def verify_collection_integrity(self, force: bool = False) -> None:
+        """Validate all RDS groups for all PI codes in the collection.
+
+        Always call this method first when defining public methods.
+
+        Args:
+            force: [OPTIONAL] If True, validates everything all over again; Even if it's already
+                been validated.
+
+        Raises:
+            RDSIntegrityFailure: The RDS block has failed its integrity check.
+            RDSPICodeMismatch: An internal PI code mismatch was detected.
+            TypeError: Invalid data type.
+            ValueError: Invalid value.
+        """
+        validate_type(force, 'force', bool)
+        self._validate_internals(force=force)
+
+    # CLASS HELPER METHODS
+    # Methods listed in alphabetical order
+
+    def _add_rds_group(self, rds_group: RDSGroup, var_name: str) -> None:
+        """SPOT on how to add an RDSGroup object to the PI code dictionary.
+
+        Args:
+            rds_group: An RDSGroup object associated with the established PI code.
+            var_name: The argument name that was the catalyst for the RDSGroup object
+                (e.g., group_bytes, rds_group).
+
+        Raises:
+            RDSIntegrityFailure: The RDS block bytes provided has failed the integrity check.
+            RDSPICodeMismatch: An internal PI code mismatch was detected.
+            TypeError: Invalid data type.
+            ValueError: Invalid value.
+        """
+        # LOCAL VARIABLES
+        rds_group_pic = None  # The PI code of rds_group
+
+        # VALIDATION
+        self.verify_collection_integrity()
+        rds_group_pic = self._fetch_internal_rds_group_picode(rds_group=rds_group,
+                                                              var_name=var_name)
+
+        # ADD IT
+        if rds_group_pic not in self._picode_dict:
+            self._picode_dict[rds_group_pic] = RDSPICode(pi_code=rds_group_pic)
+        self._picode_dict[rds_group_pic].add_rds_group(rds_group=rds_group)            
+
+    def _fetch_internal_rds_group_picode(self, rds_group: RDSGroup, var_name: str) -> bytes:
+        """Check and return an RDSGroup's PI code.
+
+        Args:
+            rds_group: The RDSGroup object to validate.
+            var_name: The argument name that was the catalyst for the RDSGroup object
+                (e.g., group_bytes, rds_group).
+
+        Returns:
+            The PI code of the RDSGroup as a bytes object.
+
+        Raises:
+            RDSIntegrityFailure: The RDS block has failed its integrity check.
+            RDSPICodeMismatch: An internal PI code mismatch was detected.
+            TypeError: Invalid data type.
+            ValueError: Invalid value.
+        """
+        # LOCAL VARIABLES
+        actual_pic = None  # The actual PI code parsed from rds_group
+
+        # VALIDATE IT
+        # Type
+        validate_type(var=rds_group, var_name=var_name, var_type=RDSGroup)
+        # Content
+        # NOTE: Calling the RDSGroup.get_group_info() method will invoke the validation method
+        try:
+            group_info = rds_group.get_group_info()  # Fetch the RDSGroupInfo object
+            group_info.validate_data()  # Ensure its validated
+            actual_pic = group_info.pic  # Get the PI code
+        except RDSIntegrityFailure as err:
+            raise RDSIntegrityFailure(f'This RDS group failed validation: {err}') from err
+
+        # DONE
+        return actual_pic
+
+    def _validate_internals(self, force: bool = False) -> None:
+        """Validate the private attributes once."""
+        if self._validated is False or force is True:
+            # self._validated
+            validate_type(var=self._validated, var_name='_validated attribute', var_type=bool)
+            # self._picode_dict
+            validate_type(var=self._picode_dict, var_name='internal dictionary', var_type=dict)
+            for pi_code, rds_pi_code_obj in self._picode_dict.items():
+                rds_pi_code_obj.verify_pi_code_integrity(force=force)
+                if pi_code != rds_pi_code_obj.get_pi_code():
+                    raise RDSPICodeMismatch('This RDS collection detected an  mismatch '
+                                            f'between the internal dictionary key "{pi_code}" and '
+                                            'the PI code of the RDSPICode PI code of '
+                                            f'"{rds_pi_code_obj.get_pi_code()}"')
+
+            self._validated = True

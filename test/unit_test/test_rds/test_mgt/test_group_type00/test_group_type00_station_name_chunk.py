f5f42ed224e3cdcd67a95b9d0eb42deef3aca7ff
hark130 <hark130@yahoo.com>
2026-01-12 09:54:17 -0600

    GAIN-5: Initial commit of MsgGroupType test pkg
    Initial commit of a unit test framework for the RDSMsgGroupType()
    dataclass sub-classes.  Also, specifically added a sub-test-class for
    the RDSMsgGroupType00() data class.
    
    There's a BUG in how the station_name_chunk property is being formed.
    The parse-and-print was coming up with garbage but, after a live KONO
    101.1 FM capture-and-decode using gallant_input, I discovered a BUG.
    
    All of this work is a TDD-style effort to fix that BUG and ensure it
    stays fixed.  (While also setting the stage to validate the parsing of
    any other RDS data into dataclasses.
    
    Meanwhile, I haven't run this yet.  It's frought with copy/pastisms (but
    that's the least of my worries), missing imports, (likely) typos, linter
    complaints, etc.  I just wanted to get the initial commit in before I
    start making it work.  This is likely a 95% working solution.
    
    > Let us see.
    -Mrs. H
    
    Changes to be committed:
    	new file:   test/unit_test/test_rds/test_mgt/__init__.py
    	new file:   test/unit_test/test_rds/test_mgt/__main__.py
    	new file:   test/unit_test/test_rds/test_mgt/test_group_type00/__init__.py
    	new file:   test/unit_test/test_rds/test_mgt/test_group_type00/__main__.py
    	new file:   test/unit_test/test_rds/test_mgt/test_group_type00/test_group_type00.py
    	new file:   test/unit_test/test_rds/test_mgt/test_group_type00/test_group_type00_station_name_chunk.py
    	new file:   test/unit_test/test_rds/test_mgt/test_mgt.py

---
+++ test/unit_test/test_rds/test_mgt/test_group_type00/test_group_type00_station_name_chunk.py
@@ -0,0 +1,121 @@
+"""Unit test module for RDSMsgGroupType00.station_name_chunk property.
+
+Typical Usage:
+    python -m test                                           # Run *all* the test cases
+    python -m test.unit_test                                 # Run *all* the unit test cases
+    python -m test.unit_test.test_rds                        # Run *all* rds sub-package test cases
+    python -m test.unit_test.test_rds.test_mgt               # Run *all* RDSMsgGroupType method test cases
+    # Run just the RDSMsgGroupType00 unit tests
+    python -m test.unit_test.test_rds.test_mgt.test_group_type00
+    # Run just the RDSMsgGroupType00.station_name_chunk test cases
+    python -m test.unit_test.test_rds.test_mgt.test_group_type00.test_group_type_station_name_chunk
+"""
+
+# Standard Imports
+from typing import Any
+# Third Party Imports
+from tediousstart.tediousstart import execute_test_cases
+# Local Imports
+from gallant_input.rds.constants import RDS_GROUP_LEN
+from gallant_input.rds.exceptions import RDSIntegrityFailure
+from gallant_input.rds.group import RDSGroup
+from gallant_input.rds.group_info import RDSGroupInfo
+from gallant_input.rds.rbds_program_type import RBDSProgTypeCode
+from test.unit_test.test_rds.test_rds_group.test_rds_group import RDSGroupUnitTest
+
+
+class RDSMsgGrpType00SNCUnitTest(RDSMsgGrpType00UnitTest):
+    """Parent class for all RDSMsgGroupType00.station_name_chunk property unit tests.
+
+    Attributes:
+        built_rds_group:    # RDSGroup() object for this test case
+        test_case_data:     # Stores data about the test case as a TestCaseData object
+        test_input_dir:     # Default input directory (OPTIONAL)
+        test_output_dir:    # Default output directory (OPTIONAL)
+    """
+
+    # CORE CLASS METHODS
+    # Methods listed in call order
+
+    def call_callable(self):
+        """Defines how the class will invoke the method call."""
+        self.get_rds_group_msg_group00(rds_group=self.input_rds_group,
+                                       assume_na=self.input_assume_na)
+        return self.self.built_rds_mgt00.station_name_chunk
+
+    def validate_return_value(self, return_value):
+        """Defines how the class will validate the return value of the tested call."""
+        self._validate_return_value(return_value=return_value)
+        if isinstance(return_value, type(self._exp_return)):
+            # Actual data types are checked in self._validate_return_value()
+            # I want to explicitly know when there's a length mismatch
+            # No need to compare the length if they're different data types
+            if len(self._exp_return) != len(return_value):
+                self._add_test_failure(f'Expected return value length of "{len(self._exp_return)}"'
+                                       f' but received a length of "{len(return_value)}" instead')
+
+    # COMMON-USE METHODS
+    # Methods listed in alphabetical order
+
+    def run_test_exception(self, rds_group: bytes, assume_na: bool,
+                           exception_type: Exception, exception_msg: str) -> None:
+        """Common method calls for a test case expected to raise an exception.
+
+        Args:
+            rds_group: Sets the rds_group argument input.  Input should be valid and well-formed.
+            assume_na: Sets the assume_na argument input.  Input should be valid and well-formed.
+            exception_type: An Exception type to expect (e.g., ValueError).
+            exception_msg: A sub-string, empty or not, to look for in the raised Exception.
+        """
+        self.set_ctor_args(rds_group=rds_group, assume_na=assume_na)
+        self.set_test_input()  # This method does not take any args
+        self.expect_exception(exception_type=exception_type, exception_msg=exception_msg)
+        self.run_test()
+
+    def run_test_return(self, rds_group: bytes, assume_na: bool, exp_ret: str) -> None:
+        """Common method calls for a test case expected to return.
+
+        Args:
+            rds_group: Sets the rds_group argument input.  Input should be valid and well-formed.
+            assume_na: Sets the assume_na argument input.  Input should be valid and well-formed.
+            exp_ret: Expected value of the dataclass property.
+        """
+        self.set_ctor_args(rds_group=rds_group, assume_na=assume_na)
+        self.set_test_input()  # This dataclass property does not take any args
+        self.expect_return(exp_ret)
+        self.run_test()
+
+    # CLASS HELPER METHODS
+    # Methods listed in alphabetical order
+
+
+class NormalRDSMsgGrpType00SNCUnitTest(RDSMsgGrpType00SNCUnitTest):
+    """Normal Test Cases."""
+
+    # TEST CASES
+    # Test cases listed in numberical order
+
+    def test_n01_good_group1_offset_0(self):
+        """Live capture of a coherent RDS Group 1: Station Name Offset 0."""
+        rds_group = self.GOOD_GROUP1_MSG00_OFF0
+        assume_na = True
+        exp_ret = '  '
+        self.run_test_return(rds_group, assume_na, exp_ret)
+
+    def test_n02_good_group2_offset_1(self):
+        """Live capture of a coherent RDS Group 2: Station Name Offset 0."""
+        rds_group = self.GOOD_GROUP2_MSG00_OFF0
+        assume_na = True
+        exp_ret = 'KO'
+        self.run_test_return(rds_group, assume_na, exp_ret)
+
+    def test_n03_good_group2_offset_1(self):
+        """Live capture of a coherent RDS Group 2: Station Name Offset 1."""
+        rds_group = self.GOOD_GROUP2_MSG00_OFF1
+        assume_na = True
+        exp_ret = 'NO'
+        self.run_test_return(rds_group, assume_na, exp_ret)
+
+
+if __name__ == '__main__':
+    execute_test_cases()
---
+++ test/unit_test/test_rds/test_mgt/test_mgt.py
@@ -0,0 +1,141 @@
+"""Defines the root rds.mgt sub-package unit test class.
+
+RDSMsgGrpTypeUnitTest is the parent class for all rds.mgt sub-package related unit test classes.
+
+    Typical usage example:
+
+    from my_module_to_test import my_function_to_test as my_function
+    from test.unit_test.test_rds.test_mgt.test_mgt import RDSMsgGrpTypeUnitTest
+
+    class RDSSomethingUnitTest(RDSMsgGrpTypeUnitTest):
+        # Establish the local test (input/output) dirs
+        def __init__(self, *args, **kwargs) -> None:
+        # Default directory for input files
+        self.test_input_dir = os.path.join(HERE, 'test_input')
+        # Default directory for output files
+        self.test_output_dir = os.path.join(HERE, 'test_output')
+
+        # Child class must override this method
+        def call_callable(self):
+            return my_function(*self._args, **self._kwargs)
+
+        # Child class must override this method
+        def validate_return_value(self):
+            self._validate_return_value(return_value=return_value)
+
+        # This is your test case
+        def test_stuff(self):
+            self.set_test_input(1, 2)
+            self.expect_return(3)
+"""
+
+# Standard Imports
+# Third Party Imports
+from test.unit_test.root_unit_test import RootUnitTest
+# Local Imports
+
+
+class RDSMsgGrpTypeUnitTest(RDSUnitTest):
+    """Parent class for all rds.mgt sub-package specific unit tests.
+
+    Inherit from this class, define necessary functionality for the function you're testing and
+    be sure to override the following methods in your child class:
+        call_callable()
+        validate_return_value()
+
+    Available features:
+        See: help(TediousUnitTest)
+
+    Attributes:
+        built_rds_group:    # RDSGroup() object for this test case
+        test_case_data:     # Stores data about the test case as a TestCaseData object
+        test_input_dir:     # Default input directory (OPTIONAL)
+        test_output_dir:    # Default output directory (OPTIONAL)
+    """
+
+    # GOOD BLOCK VALUES
+    # GOOD GROUP 1: KONO 101.1 FM Live Capture of Group Type 00A - Station Name "    "
+    GOOD_GRP1_MSG00_BLOCK_A0 = bytes('00110110010110001000011011', 'utf-8')  # PICode
+    GOOD_GRP1_MSG00_BLOCK_B0 = bytes('00000000111011001101011000', 'utf-8')  # Metadata
+    GOOD_GRP1_MSG00_BLOCK_C0 = bytes('11100001100010001111100110', 'utf-8')  # Alt Freq (AF)
+    GOOD_GRP1_MSG00_BLOCK_D0 = bytes('00100000001000000011011100', 'utf-8')  # Offset 0: "  "
+    # GOOD GROUP 2: KONO 101.1 FM Live Capture of Group Type 00A - Station Name "KONO" Offset 0
+    GOOD_GRP2_MSG00_BLOCK_A0 = bytes('00110110010110001000011011', 'utf-8')  # PICode
+    GOOD_GRP2_MSG00_BLOCK_B0 = bytes('00000000111011001101011000', 'utf-8')  # Metadata
+    GOOD_GRP2_MSG00_BLOCK_C0 = bytes('11100001100010001111100110', 'utf-8')  # Alt Freq (AF)
+    GOOD_GRP2_MSG00_BLOCK_D0 = bytes('01001011010011111111001100', 'utf-8')  # Offset 0: "KO"
+    # GOOD GROUP 3: KONO 101.1 FM Live Capture of Group Type 00A - Station Name "KONO"
+    GOOD_GRP3_MSG00_BLOCK_A1 = bytes('00110110010110001000011011', 'utf-8')  # PICode
+    GOOD_GRP3_MSG00_BLOCK_B1 = bytes('00000000111010010110111100', 'utf-8')  # Metadata
+    GOOD_GRP3_MSG00_BLOCK_C1 = bytes('11100001100010001111100110', 'utf-8')  # Alt Freq (AF)
+    GOOD_GRP3_MSG00_BLOCK_D1 = bytes('01001110010011110001100000', 'utf-8')  # Offset 1: "NO"
+
+    # GOOD GROUP VALUES
+    # KONO 101.1 FM Live Capture of Group Type 00A - Station Name
+    # GOOD GROUP 1: Station Name "    " Offset 0 "  "
+    GOOD_GROUP1_MSG00_OFF0 = GOOD_GRP1_MSG00_BLOCK_A + GOOD_GRP1_MSG00_BLOCK_B \
+                             + GOOD_GRP1_MSG00_BLOCK_C + GOOD_GRP1_MSG00_BLOCK_D
+    # GOOD GROUP 2: Station Name "KONO" Offset 0 "KO"
+    GOOD_GROUP2_MSG00_OFF0 = GOOD_GRP2_MSG00_BLOCK_A0 + GOOD_GRP2_MSG00_BLOCK_B0 \
+                             + GOOD_GRP2_MSG00_BLOCK_C0 + GOOD_GRP2_MSG00_BLOCK_D0
+    # GOOD GROUP 2: Station Name "KONO" Offset 1 "NO"
+    GOOD_GROUP2_MSG00_OFF1 = GOOD_GRP3_MSG00_BLOCK_A0 + GOOD_GRP3_MSG00_BLOCK_B0 \
+                             + GOOD_GRP3_MSG00_BLOCK_C0 + GOOD_GRP3_MSG00_BLOCK_D0
+
+    # CORE CLASS METHODS
+    # Methods listed in call order
+
+    def __init__(self, *args, **kwargs) -> None:
+        """RDSBlockUnitTest ctor."""
+        # ATTRIBUTES
+        self.built_rds_group = None  # RDSGroup() object built from input
+
+        super().__init__(*args, **kwargs)
+
+    def call_callable(self):
+        """Defines how the class will invoke the function call.
+
+        Child class must override this method.  See TediousUnitTest.call_callable() for details.
+        """
+        # Example Usage:
+        # return the_function_you_are_testing(*self._args, **self._kwargs)
+        raise NotImplementedError(
+            self._test_error.format('The child class must override the call_callable method with '
+                                    'the function to test.'))
+
+    def validate_return_value(self, return_value):
+        """Defines how the class will validate the return value of the tested call.
+
+        Child class must override this method.
+        See TediousUnitTest.validate_return_value() for details.
+        """
+        # Example Usage:
+        # self._validate_return_value(return_value=return_value)
+        raise NotImplementedError(
+            self._test_error.format('The child class must override the validate_return_value '
+                                    'method with the appropriate validation logic'))
+
+    # COMMON-USE METHODS
+    # Methods listed in alphabetical order
+
+    def build_rds_group(self, rds_group: bytes, assume_na: bool = True) -> None:
+        """Build an RDSGroup() object to fetch RDSMsgGroupType()s with."""
+        try:
+            self.built_rds_group = RDSGroup(rds_group=rds_group, assume_na=assume_na)
+            self.built_rds_group.verify_group_integrity()
+        except (RDSIntegrityFailure, TypeError, ValueError) as err:
+            self.fail_test_case(f'Invalid RDSGroup({rds_group}, {assume_na}) '
+                                f'test case input: {repr(err)}')
+
+    def set_ctor_args(self, rds_group: bytes, assume_na: bool) -> None:
+        """Set the RDSGroup() ctor arguments.
+
+        Args:
+            rds_group: Sets the rds_group argument input.  Input should be valid and well-formed.
+            assume_na: Sets the assume_na argument input.  Input should be valid and well-formed.
+        """
+        self.input_rds_group = rds_group
+        self.input_assume_na = assume_na
+
+    # CLASS HELPER METHODS
+    # Methods listed in alphabetical order

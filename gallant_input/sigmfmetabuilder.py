f202ddb2c1d5bf59d2e572c026704329fd9de2e3
hark130 <hark130@yahoo.com>
2026-03-10 10:52:05 -0500

    GAIN-10: Big sigmfmetabuilder module update
    - Stubbed out build_annotations_array()
    - Defined build_captures_array()
    - Defined build_default_metadata()
    - Fixed some type hints
    - Updated some docstrings
    - Added details to some Exception messages
    - Created a local SPOT for some SigMF limits (better, not best)
    
    EOD(ish) commit (I have a meeting to get to)
    
    TO DO: DON'T DO NOW:
    - Implement build_annotations_array()
    - Finish implementing build_default_metadata()
    - Start work on the io module
    
    Changes to be committed:
    	modified:   gallant_input/sigmfmetabuilder.py
    
    Untracked files:
    	gallant_input/io.py

--- gallant_input/sigmfmetabuilder.py
+++ gallant_input/sigmfmetabuilder.py
@@ -1,15 +1,74 @@
 """Functionality to easily build up SigMF metadata."""
 
 # Standard Imports
+from typing import Any, List
 # Third Party Imports
 import sigmf
 # Local Imports
-from gallant_input.constants import (SIG_GLOB_AUTHOR_KEY, SIG_GLOB_DATATYPE_KEY, DEF_USERNAME,
+from gallant_input.constants import (SIG_CAP_DATETIME_KEY, SIG_CAP_FREQUENCY_KEY,
+                                     SIG_CAP_START_INDEX_KEY, SIG_GLOB_AUTHOR_KEY,
+                                     SIG_FIELD_ANNOTATION_KEY, SIG_FIELD_CAPTURE_KEY,
+                                     SIG_FIELD_GLOBAL_KEY, SIG_GLOB_DATATYPE_KEY, DEF_USERNAME,
                                      SIG_GLOB_DESCRIPTION_KEY, SIG_GLOB_SAMPLE_RATE_KEY,
                                      SIG_GLOB_VERSION_KEY)
 from gallant_input.sigmfdatatype import SigMFDataType
 from gallant_input.validation import (validate_bool, validate_int, validate_int_or_float,
                                       validate_string, validate_type)
+
+
+def build_annotations_array() -> List:
+    """TO DO: DON'T DO NOW..."""
+
+
+def build_captures_array(sample_start: int = 0, date_time: str | None = None,
+                         center_freq: float | None = None) -> List[dict[str:Any]]:
+    """Build a SigMF captures array list for use as a 'captures' value in the metadata dictionary.
+
+    See: https://sigmf.org/#subsec:CapturesArray
+
+    Args:
+        sample_start: [OPTIONAL] Index of first sample of this chunk.
+            This value may not be None because it is required.
+        date_time: [OPTIONAL] An ISO-8601 string indicating the timestamp of the sample index
+            specified by sample_start.  Use spacetime.create_rfc_3339_z_time() to generate a valid
+            string.
+        center_freq: [OPTIONAL] The center frequency of the signal in Hz.
+
+    Returns:
+        A captures array list of dictionaries with sigmf.SigMFFile.*_KEY as keys with the
+        arguments as values.
+
+    Raises:
+        TypeError: Bad data type.
+        ValueError: Invalid value.
+    """
+    # LOCAL VARIABLES
+    captures_dict = {}                   # SigMF Captures object dict of SigMFFile.*_KEY keys/vals
+    max_sam_start = 9223372036854775807  # Maximuium value for sample start
+    min_cen_freq = -1000000000000        # Minimum value for center_freq
+    max_cen_freq = 1000000000000         # Maximum value for center_freq
+
+    # BUILD IT
+    # sample_start
+    validate_int(sample_start, 'sample_start')
+    if sample_start < 0 or sample_start > max_sam_start:
+        raise ValueError(f'The "sample_start" value of "{sample_start}" violates the SigMF '
+                         f'standard of minimum : 0 maximum : {max_sam_start}')
+    captures_dict[SIG_CAP_START_INDEX_KEY] = sample_start
+    # date_time
+    if date_time is not None:
+        validate_string(date_time, 'date_time', can_be_empty=False)
+        captures_dict[SIG_CAP_DATETIME_KEY] = date_time
+    # center_freq
+    if center_freq is not None:
+        validate_float(center_freq, 'center_freq')
+        if center_freq < min_cen_freq or center_freq > max_cen_freq:
+            raise ValueError(f'The "center_freq" value of "{center_freq}" violates the SigMF '
+                             f'standard of minimum : {min_cen_freq} maximum : {max_cen_freq}')
+        captures_dict[SIG_CAP_FREQUENCY_KEY] = center_freq
+
+    # DONE
+    return captures_dict
 
 
 def build_dataset_format(is_complex: bool = True, data_type: SigMFDataType = SigMFDataType.FLOAT,
@@ -70,10 +129,49 @@ def build_dataset_format(is_complex: bool = True, data_type: SigMFDataType = Sig
     return data_format
 
 
+def build_default_metadata(dataset_format: str = 'cf32_le', samp_rate: int | float | None = None,
+                           date_time: str | None = None,
+                           center_freq: float | None = None) -> dict[str:Any]
+    """Build a basic SigMF metadata dictionary.
+
+    Creates a basic dictionary with a global object, captures array, and an annotations array.
+
+    Args:
+        dataset_format: [OPTIONAL] A non-empty string w/ a SigMF Dataset Format that conforms with
+            Augmented Backus-Naur form (ABNF) rules.  Use build_dataset_format() to build this str.
+            This value may not be None because it is required.
+        samp_rate: [OPTIONAL] The sampling frequency in Hz.
+        date_time: [OPTIONAL] An ISO-8601 string indicating the timestamp of the sample index
+            specified by sample_start.  Use spacetime.create_rfc_3339_z_time() to generate a valid
+            string.
+        center_freq: [OPTIONAL] The center frequency of the signal in Hz.
+
+    Returns:
+        A basic SigMF dictionary containing a global object, captures array, and an
+        annotations array.
+
+    Raises:
+        TypeError: Bad data type.
+        ValueError: Invalid value.
+    """
+    # LOCAL VARIABLES
+    metadata_dict = {}  # The SigMF metadata dictionary
+
+    # BUILD IT
+    metadata_dict[SIG_FIELD_GLOBAL_KEY] = build_global_object(dataset_format=dataset_format,
+                                                              samp_rate=samp_rate)
+    metadata_dict[SIG_FIELD_CAPTURE_KEY] = build_captures_array(date_time=date_time,
+                                                                center_freq=center_freq)
+    metadata_dict[SIG_FIELD_ANNOTATION_KEY] = build_annotations_array()  # TO DO: DON'T DO NOW... Finish this after the function is implemented
+
+    # DONE
+    return metadata_dict
+
+
 def build_global_object(dataset_format: str = 'cf32_le', samp_rate: int | float | None = None,
                         author: str | None = DEF_USERNAME, description: str | None = None,
-                        version: str = sigmf.__version__) -> dict[str:str]:
-    """Build a SigMF global object dictionary.
+                        version: str = sigmf.__version__) -> dict[str:Any]:
+    """Build a SigMF global object dictionary for use as a 'global' value in the metadata dict.
 
     See: https://sigmf.org/#subsec:GlobalObject
 
@@ -89,14 +187,15 @@ def build_global_object(dataset_format: str = 'cf32_le', samp_rate: int | float 
             Metadata file, in the X.Y.Z format.  This value may not be None because it is required.
 
     Returns:
-        A dictionary of sigmf.SigMFFile.*_KEY as keys with the arguments as values.
+        A global object dictionary of sigmf.SigMFFile.*_KEY as keys with the arguments as values.
 
     Raises:
         TypeError: Bad data type.
         ValueError: Invalid value.
     """
     # LOCAL VARIABLES
-    global_dict = {}  # The SigMF Global object dictionary of SigMFFile.*_KEY key/value pairs.
+    global_dict = {}               # SigMF Global object dict of SigMFFile.*_KEY key/value pairs
+    max_samp_rate = 1000000000000  # Maximum value for samp_rate
 
     # BUILD IT
     # dataset_format
@@ -105,9 +204,9 @@ def build_global_object(dataset_format: str = 'cf32_le', samp_rate: int | float 
     # samp_rate
     if samp_rate is not None:
         validate_int_or_float(samp_rate, 'samp_rate')
-        if samp_rate < 0 or samp_rate > 1000000000000:
+        if samp_rate < 0 or samp_rate > max_samp_rate:
             raise ValueError(f'The "samp_rate" value of "{samp_rate}" violates the SigMF standard '
-                             'of minimum : 0 maximum : 1000000000000')
+                             f'of minimum : 0 maximum : {max_samp_rate}')
         global_dict[SIG_GLOB_SAMPLE_RATE_KEY] = samp_rate
     # author
     if author is not None:

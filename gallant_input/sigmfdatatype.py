24df5423447e4fb088e738375281d0d384695951
hark130 <hark130@yahoo.com>
2026-03-10 06:45:08 -0500

    GAIN-10: Initial commit of SigMFDataType Int Enum
    This IntEnum will be used as an argument data type for
    build_dataset_format()
    
    Changes to be committed:
    	new file:   gallant_input/sigmfdatatype.py
    
    Untracked files:
    	gallant_input/io.py
    	gallant_input/sigmfmetabuilder.py

---
+++ gallant_input/sigmfdatatype.py
@@ -0,0 +1,41 @@
+"""Defines the SigMFDataType enum for SigMF Dataset Format data types.
+
+See: https://sigmf.org/#sigmf-dataset-format
+"""
+
+# Standard Imports
+from enum import IntEnum
+from typing import Final
+# Third Party Imports
+# Local Imports
+
+# PRIVATE CONSTANTS
+_UNDEFINED: Final[int] = 0  # No SigMF dataset format type or undefined
+_FLOAT: Final[int] = 1      # Floating-point:   'f'
+_INT: Final[int] = 2        # Signed-integer:   'i'
+_UINT: Final[int] = 3       # Unsigned-integer: 'u'
+
+
+class SigMFDataType(IntEnum):
+    """SigMF Dataset Format data types."""
+    UNDEFINED = _UNDEFINED  # No SigMF dataset format type or undefined
+    FLOAT = _FLOAT          # Floating-point:   'f'
+    INT = _INT              # Signed-integer:   'i'
+    UINT = _UINT            # Unsigned-integer: 'u'
+
+    @property
+    def translate(self) -> str:
+        """Convert the IntEnum.value into its SigMF code: 'f', 'i', or 'u'."""
+        sigmf_code = None  # The IntEnum value converted to SigMF Dataset Format data type code
+        if self.value == _FLOAT:
+            sigmf_code = 'f'
+        elif self.value == _INT:
+            sigmf_code = 'i'
+        elif self.value == _UINT:
+            sigmf_code = 'u'
+        elif self.value == _UNDEFINED:
+            raise LookupError(f'No SigMF code exists for "{self.name.title()}"')
+        else:
+            raise NotImplementedError('This property method has not yet added support '
+                                      f'for "{self.name.title()}"')
+        return sigmf_code

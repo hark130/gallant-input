358062974b8df847e789dfd62c04ffab88760bf4
hark130 <hark130@yahoo.com>
2026-03-13 08:47:36 -0500

    GAIN-10: Initial commit of sigmf test framework
    Includes...
    - base sigmf test class
    - base SigMFDTypeInfo test class
    - 48 SigMFDtypeInfo.bit_width test cases
    
    45 of the bit_width test cases are failing for valid reasons.
    Now that the test cases are written, I'll implement the bit_width
    property method (TDD-style).
    Then, I'll get back to SigMFMetaParser and then the read_sigmf_samples()
    functionality in the io module.
    
    Changes to be committed:
    	new file:   test/unit_test/test_sigmf/__init__.py
    	new file:   test/unit_test/test_sigmf/__main__.py
    	new file:   test/unit_test/test_sigmf/test_dtypeinfo/__init__.py
    	new file:   test/unit_test/test_sigmf/test_dtypeinfo/__main__.py
    	new file:   test/unit_test/test_sigmf/test_dtypeinfo/test_dtypeinfo.py
    	new file:   test/unit_test/test_sigmf/test_dtypeinfo/test_dtypeinfo_bit_width.py
    	new file:   test/unit_test/test_sigmf/test_sigmf.py
    
    Changes not staged for commit:
    	modified:   gallant_input/io.py
    	modified:   gallant_input/sigmfmetaparser.py

---
+++ test/unit_test/test_sigmf/test_dtypeinfo/__init__.py
@@ -0,0 +1,3 @@
+from test.unit_test.test_sigmf.test_dtypeinfo.test_dtypeinfo import SigMFDTypeInfoUnitTest
+
+__all__ = ['SigMFDTypeInfoUnitTest']
---
+++ test/unit_test/test_sigmf/test_dtypeinfo/test_dtypeinfo.py
@@ -0,0 +1,92 @@
+"""Defines the root SigMFDTypeInfo unit test class.
+
+SigMFDTypeInfoUnitTest is the parent class for all ais.block SigMFDTypeInfo unit test classes.
+
+    Typical usage example:
+
+    from my_module_to_test import my_function_to_test as my_function
+    from test.unit_test.test_ais.test_ais_block.test_ais_block import SigMFDTypeInfoUnitTest
+
+    class SigMFDTypeInfoFuncUnitTest(SigMFDTypeInfoUnitTest):
+        # Establish the local test (input/output) dirs
+        def __init__(self, *args, **kwargs) -> None:
+        # Default directory for input files
+        self.test_input_dir = os.path.join(HERE, 'test_input')
+        # Default directory for output files
+        self.test_output_dir = os.path.join(HERE, 'test_output')
+
+        # Child class must override this method
+        def call_callable(self):
+            return my_function(*self._args, **self._kwargs)
+
+        # Child class must override this method
+        def validate_return_value(self):
+            self._validate_return_value(return_value=return_value)
+
+        # This is your test case
+        def test_stuff(self):
+            self.set_test_input(1, 2)
+            self.expect_return(3)
+"""
+
+# Standard Imports
+from typing import Any
+# Third Party Imports
+from test.unit_test.test_sigmf.test_sigmf import SigMFUnitTest
+# Local Imports
+from gallant_input.sigmfdtypeinfo import SigMFDTypeInfo
+
+
+class SigMFDTypeInfoUnitTest(SigMFUnitTest):
+    """Parent class for all SigMFDTypeInfo method unit tests.
+
+    Inherit from this class, define necessary functionality for the function you're testing and
+    be sure to override the following methods in your child class:
+        call_callable()
+        validate_return_value()
+
+    Available features:
+        See: help(TediousUnitTest)
+
+    Attributes:
+        test_case_data:      # Stores data about the test case as a TestCaseData object
+        test_input_dir:      # Default input directory (OPTIONAL)
+        test_output_dir:     # Default output directory (OPTIONAL)
+    """
+
+    # CORE CLASS METHODS
+    # Methods listed in call order
+
+    def call_callable(self):
+        """Defines how the class will invoke the function call.
+
+        Child class must override this method.  See TediousUnitTest.call_callable() for details.
+        """
+        # Example Usage:
+        # test_obj = SigMFDTypeInfo(...)
+        # return test_obj.the_method_you_are_testing(*self._args, **self._kwargs)
+        raise NotImplementedError(
+            self._test_error.format('The child class must override the call_callable method with '
+                                    'the function to test.'))
+
+    def validate_return_value(self, return_value):
+        """Defines how the class will validate the return value of the tested call.
+
+        Child class must override this method.
+        See TediousUnitTest.validate_return_value() for details.
+        """
+        # Example Usage:
+        # self._validate_return_value(return_value=return_value)
+        raise NotImplementedError(
+            self._test_error.format('The child class must override the validate_return_value '
+                                    'method with the appropriate validation logic'))
+
+    # COMMON-USE METHODS
+    # Methods listed in alphabetical order
+
+    def create_test_obj(self) -> SigMFDTypeInfo:
+        """Create a SigMFDTypeInfo() test object."""
+        return SigMFDTypeInfo(*self._args, **self._kwargs)
+
+    # CLASS HELPER METHODS
+    # Methods listed in alphabetical order

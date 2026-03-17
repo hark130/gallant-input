857780f7e6b0b389a1e1529fe6e2545fb4e20566
hark130 <hark130@yahoo.com>
2026-03-17 07:30:53 -0500

    GAIN-10: Extended Endian IntEnum comment
    Apparently, some APIs return '|'  not applicable if the endianness of a
    datatype/dataset doesn't matter (e.g., ci8).
    To help reflect that, I extended the comment of Endian.UNSPECIFIED to
    also include N/A.
    
    Changes to be committed:
    	modified:   gallant_input/endian.py
    
    Changes not staged for commit:
    	modified:   gallant_input/io.py
    	modified:   gallant_input/sigmfdtypeinfo.py
    	modified:   gallant_input/sigmfmetaparser.py
    	modified:   test/unit_test/test_sigmf/test_dtypeinfo/test_dtypeinfo_bit_width.py
    
    Untracked files:
    	test/unit_test/test_sigmf/test_dtypeinfo/test_dtypeinfo_read_dtype.py

--- gallant_input/endian.py
+++ gallant_input/endian.py
@@ -8,6 +8,6 @@ from enum import IntEnum
 
 class Endian(IntEnum):
     """Endianness."""
-    UNSPECIFIED = 0  # Unknown endianness
+    UNSPECIFIED = 0  # Unknown endianness or not applicable
     BIG = 2          # Big-endian
     LITTLE = 1       # Little-endian

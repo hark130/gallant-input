d32540aca34f83c862d7f42417aecb8e4b721c76
hark130 <hark130@yahoo.com>
2026-04-22 12:53:55 -0500

    GAIN-16: Added funcs to codec module
    Untested, unused, unlinted.
    
    Implemented:
    - convert_bytes_to_bits()
    - map_bits_to_symbols()
    - _validate_bps_to_mapper()
    
    EOD commit.
    
    Changes to be committed:
    	modified:   gallant_input/codec.py
    
    Changes not staged for commit:
    	modified:   gallant_input/modem/modem.py
    	modified:   gallant_input/validation.py
    
    Untracked files:
    	gallant_input/modem/constants.py
    	gallant_input/modem/ook.py

--- gallant_input/codec.py
+++ gallant_input/codec.py
@@ -1,9 +1,37 @@
-"""Functionaly to encode and decode data."""
+"""Functionality to encode and decode data."""
 
 # Standard Imports
+import math
 # Third Party Imports
+import numpy
 # Local Imports
-from gallant_input.validation import validate_binary_bytes
+from gallant_input.validation import validate_binary_bytes, validate_float_or_complex
+
+
+def convert_bytes_to_bits(bin_bytes: bytes) -> numpy.ndarray:
+    """Convert a bytes object containing binary data to an array object.
+
+    Args:
+        bin_bytes: A bytes object containing binary binary to convert.
+
+    Returns:
+        The converted bin_bytes value.
+
+    Raises:
+        TypeError: Invalid data type.
+        ValueError: Bad value (e.g., "...and I thought I saw a 2" -Bender).
+    """
+    # LOCAL VARIABLES
+    bit_array = None  # Converted bytes to bit array
+
+    # INPUT VALIDATION
+    validate_binary_bytes(validate_this=bin_bytes, param_name='bin_bytes', exact_len=None)
+
+    # CONVERT IT
+    bit_array = numpy.unpackbits(numpy.frombuffer(bin_bytes, dtype=numpy.uint8))
+
+    # DONE
+    return bit_array
 
 
 def decode_differential_binary(bin_bytes: bytes) -> bytes:
@@ -34,3 +62,56 @@ def decode_differential_binary(bin_bytes: bytes) -> bytes:
 
     # DONE
     return decoded
+
+
+def map_bits_to_symbols(bitstream: numpy.ndarray, bits_per_symbol: int,
+                        mapper: dict[int, float | complex]) -> np.ndarray:
+    """Map binary data to symbols according to the mapper.
+
+    The number of entries in mapper must equal 2^bits_per_symbol.
+
+    Args:
+        bitstream: An array of binary data to map to symbols.
+        bits_per_symbol: The number of bits included in each symbol.
+        mapper: The bits --> symbol dictionary.
+    """
+    # LOCAL VARIABLES
+    pad = 0         # The number of bits to pad to bitstream
+    grouped = None  # The padded bitstream grouped into bits_per_symbol chunks
+    ints = None     # Bits converted to integers
+    mapping = None  # Bitstream mapped to mapper values
+
+    # VALIDATION
+    validate_ndarray(array=bitstream, array_name='bitstream', can_be_empty=False,
+                     num_dim=None, must_be_complex=False)
+    _validate_bps_to_mapper(bits_per_symbol, mapper)
+
+    # MAP IT
+    # Pad
+    pad = (-len(bitstream)) % bits_per_symbol
+    if pad:
+        bitstream = numpy.concatenate([bitstream, numpy.zeros(pad, dtype=numpy.uint8)])  # Add zeros
+    # Reshape
+    grouped = bitstream.reshape(-1, bits_per_symbol)
+    # Convert bits to integers
+    ints = grouped.dot(1 << numpy.arange(bits_per_symbol)[::-1])
+    # Map to symbols
+    mapping = numpy.array([mapper[i] for i in ints])
+
+    # DONE
+    return mapping
+
+
+def _validate_bps_to_mapper(bits_per_symbol: int, mapper: dict[int, float | complex]) -> None:
+    """Validate these arguments under their own strength and against each other."""
+    # VALIDATION
+    validate_pos_int(bits_per_symbol, 'bits_per_symbol')
+    validate_type(mapper, 'mapper', dict)
+    if len(mapper) != math.pow(2, bits_per_symbol):
+        raise ValueError(f'The length of the "mapper" dictionary ({len(mapper)}) does not equal '
+                         f'2^{bits_per_symbol}')
+    for key, value in mapper:
+        validate_int(key, 'a key in the mapper dictionary')
+        if key < 0:
+            raise ValueError(f'Keys in the "mapper" dictionary may not be negative: {key}')
+        validate_float_or_complex(value, 'a value in the mapper dictionary')

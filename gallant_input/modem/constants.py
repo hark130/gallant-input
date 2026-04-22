c20216fe2330af92dffe1041260bb587fe09c573
hark130 <hark130@yahoo.com>
2026-04-22 12:56:52 -0500

    GAIN-16: Initial commit of modem.constants
    Initial commit of sub-package "modem" specific constants
    
    Changes to be committed:
    	new file:   gallant_input/modem/constants.py
    
    Untracked files:
    	gallant_input/modem/ook.py

---
+++ gallant_input/modem/constants.py
@@ -0,0 +1,14 @@
+"""Defines modem specific constant values."""
+
+# Standard Imports
+from typing import Final
+# Third Party Imports
+# Local Imports
+
+
+# DEFAULT BITS --> SYMBOL MAPPINGS
+# Amplitude Modulation (AM)
+OOK_MAP: Final[dict[int, float]] = {0: 0.0, 1: 1.0}
+# Phase Modulation (PM)
+BPSK_MAP: Final[dict[int, complex]] = {0: -1+0j, 1: 1+0j}
+QPSK_MAP: Final[dict[int, complex]] = {0b00: 1+1j, 0b01: -1+1j, 0b10: 1-1j, 0b11: -1-1j}

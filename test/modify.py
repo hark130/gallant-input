8dbbef71d79f9a63ded344e390bdea89b9f9893d
hark130 <hark130@yahoo.com>
2026-05-05 13:37:07 -0500

    GAIN-16: Defined add_awgn() in test.modify
    It's working(?) well(?) and helped highlight a test case oddity
    
    Changes to be committed:
    	modified:   test/modify.py
    
    Changes not staged for commit:
    	modified:   test/comp_test/test_modem/test_modem_ook.py
    	modified:   test/unit_test/test_modem/test_ook/test_modem_ook_demodulate.py

--- test/modify.py
+++ test/modify.py
@@ -6,6 +6,55 @@ import numpy
 # Local Imports
 from gallant_input.codec import convert_ascii_bin_bytes_to_bits, upsample
 from gallant_input.modem.calc import calculate_sps
+from gallant_input.validation import validate_int_or_float, validate_ndarray
+
+
+def add_awgn(samples: numpy.ndarray, snr_db: float | int) -> numpy.ndarray:
+    """Add AWGN to samples, regardless of dtype, based on desired SNR.
+
+    Additive White Gaussian Noise (AWGN)
+    Signal-to-Noise Ratio (SNR)
+
+    Args:
+        samples: The original samples to add AWGN to.
+        snr_db: The desigred SNR, in decibels.
+
+    Returns:
+        A noisy signal, derived from samples, in an ndarray.
+
+    Raises:
+        TypeError: Bad data type.
+        ValueError: Bad value.
+    """
+    # LOCAL VARIABLES
+    rng = numpy.random.default_rng()  # Random noise generator object
+    new_samps = None                  # A copy of samples as complex128 samples
+    signal_power = None               # Original signal power
+    snr_linear = None                 # Desired noise power
+    noise_power = None                # Solve for P(noise)
+    sigma = None                      # Standard deviation of the noise
+    noise = None                      # Randomly generated AWGN
+    noisy = None                      # Noise + samples
+
+    # INPUT VALIDATION
+    validate_ndarray(array=samples, array_name='samples', can_be_empty=False, num_dim=1,
+                   must_be_complex=False)
+    validate_int_or_float(snr_db, 'snr_db')  # Ignore actual value since it could be anything
+
+    # ADD IT
+    new_samps = samples.astype(numpy.complex128)
+    signal_power = numpy.mean(numpy.abs(new_samps) ** 2)
+    if signal_power <= 0:
+        noise_power = 1e-3  # Arbitrary noise floor for a non-existent signal
+    else:
+        snr_linear = 10 ** (snr_db / 10)
+        noise_power = signal_power / snr_linear
+    sigma = numpy.sqrt(noise_power / 2)
+    noise = rng.normal(0, sigma, samples.shape) + 1j * rng.normal(0, sigma, samples.shape)
+    noisy = samples + noise
+
+    # DONE
+    return noisy.astype(samples.dtype)  # Cast back to original dtype
 
 
 def convert_bin_bytes_to_array(bin_bytes: bytes, sample_rate: int | float,

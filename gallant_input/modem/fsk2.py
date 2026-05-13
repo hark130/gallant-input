966c15e55008530ae5848370049802fdc94f2ba3
hark130 <hark130@yahoo.com>
2026-05-13 06:38:10 -0500

    GAIN-16: Initial commit of modem.fsk2 FSK2()
    Initial commit of a Modem() child class to handle 2-FSK continuous phase
    modulation.  The demodulate() method has not yet been implemented.
    No test cases have been written (that's next).  The linters haven't been
    run on it yet.
    
    However... manual testing of FSK2() (1. Modulate a binary stream, 2.
    Output the samples to IQ files, 3. Run the IQ files through URH and
    Inspectrum) makes it seem like FSK2().modulate() is working perfectly
    (with valid input).  I was able to manually decode the IQ in Inspectrum
    and URH was able to automagically decode the ASCII from the decoded
    bits.  This is a big milestone.
    
    Next step, write test cases for:
    - FSK2().modulate()
    - FSK2().demodulate()
    - FSK2().get_phase()? (though this might be best served in comp testing)
    - FSK2() component test cases
    
    Changes to be committed:
    	new file:   gallant_input/modem/fsk2.py
    
    Untracked files:
    	GAIN-16_bfsk_mod1_4800hz.iq
    	GAIN-16_bfsk_mod2_4800hz.iq
    	GAIN-16_fsk_mod1_4800hz.iq
    	GAIN-16_fsk_mod2_4800hz.iq

---
+++ gallant_input/modem/fsk2.py
@@ -0,0 +1,177 @@
+"""Defines the class for Binary Frequency Shift Key (FSK) MOdulation/DEModulation."""
+
+# Standard Imports
+import math
+# Third Party Imports
+import numpy
+# Local Imports
+from gallant_input.codec import (convert_ascii_bin_bytes_to_bits, map_bits_to_symbols,
+                                 stringify_ndarray, upsample)
+from gallant_input.modem.calc import (compute_threshold, extract_bits_from_samples,
+                                      extract_bits_from_single_cluster)
+from gallant_input.modem.constants import OOK_MAP
+from gallant_input.modem.modem import Modem
+from gallant_input.modem.threshold_scheme import ThresholdScheme
+from gallant_input.validation import validate_bool, validate_ndarray, validate_pos_float
+
+
+class FSK2(Modem):
+    """Modulate and demodulate BFSK digital signals."""
+
+    # CORE METHODS
+
+    def __init__(self, *args, **kwargs):
+        """Class ctor."""
+        self._phase = float(0.0)  # Phase state
+        super().__init__(*args, **kwargs)
+
+    # ABSTRACT METHODS
+
+    def modulate(self, bin_bytes: bytes, freq0: float | int, freq1: float,
+                 phase: float | None = None) -> numpy.ndarray:
+        """MOdulate binary data.
+
+        Args:
+            bin_bytes: A bytes object containing binary to modulate.
+            freq0: The 'off' frequency baseband deviation.
+            freq1: The 'on' frequency baseband deviation.
+            phase: [OPTIONAL] Override the internal phase continuity.
+
+        Returns:
+            The modulated binary data.
+
+        Raises:
+            TypeError: Invalid data type.
+            ValueError: Bad value (e.g., "...and I thought I saw a 2" -Bender).
+        """
+        # LOCAL VARIABLES
+        bits = None      # The binary bytes object converted at a numpy.ndarray
+        freqs = None     # The bits array converted to on/off frequencies
+        phase_inc = 0.0  # The amount to increase the phase by
+        phi = None       # The new angle
+        out = []         # Running list of samples
+        iq = None        # Final array of modulated samples
+
+        # VALIDATION
+        self.parse()  # Validate and parse
+        self._validate_frequencies(freq0=freq0, freq1=freq1)
+        bits = convert_ascii_bin_bytes_to_bits(bin_bytes)
+        if phase is not None:
+            self._update_phase(phase, pre_validate=True)
+            self._validate_phase()  # Check it again now that it has been overridden
+
+        # MODULATE IT
+        freqs = numpy.where(bits == 0, freq0, freq1)
+        for freq in freqs:
+            phase_inc = 2 * numpy.pi * freq / self.sample_rate
+            phi = self._phase + phase_inc * numpy.arange(self._sps)
+            out.append(numpy.exp(1j * phi))
+            self._update_phase(phi[-1] + phase_inc)  # Maintain a continuous phase
+        iq = numpy.concatenate(out).astype(numpy.complex64)
+
+        # DONE
+        return iq
+
+    def demodulate(self, samples: numpy.ndarray) -> bytes:
+        """DEMoodulate binary data.
+
+        Args:
+            samples: Digital samples to demodulate.
+            threshold: [OPTIONAL] Magnitude threshold used to decide between binary results.
+                If None, automatically determine the threshold.
+
+        Returns:
+            The demodulated binary data.
+
+        Raises:
+            TypeError: Invalid data type.
+            ValueError: Bad value.
+        """
+        # LOCAL VARIABLES
+        bit_stream = b''  # The bits as a bin bytes object
+
+        # VALIDATION
+        self.parse()  # Validate and parse
+
+        # DEMODULATE IT
+
+        # DONE
+        return bit_stream
+
+    # PUBLIC METHODS
+
+    def get_phase(self) -> float:
+        """Fetch the current phase."""
+        self.parse()  # Validate and parse
+        self._validate_phase()  # Check it again just to be sure
+        return self._phase
+
+    def parse(self) -> None:
+        """Validate, parse and update attributes once.
+
+        Raises:
+            TypeError: Bad data type.
+            ValueError: Badd value.
+        """
+        # VALIDATION
+        validate_bool(self._parsed, 'internal attribute _parsed')
+        self.validate()
+        # PARSE IT
+        if not self._parsed:
+            self._parse()
+            self._parsed = True
+
+    def validate(self) -> None:
+        """Validate attribute values once.
+
+        Raises:
+            TypeError: Bad data type.
+            ValueError: Badd value.
+        """
+        # VALIDATION
+        validate_bool(self._validated, 'internal attribute _validated')
+        if not self._validated:
+            self._validate()
+            self._validated = True
+
+    # PRIVATE METHODS
+
+    def _parse(self) -> None:
+        """Parse user input."""
+        self._parse_abc()
+
+    def _update_phase(self, new_phase: float, pre_validate: bool = False) -> None:
+        """Trim and update the phase attribute."""
+        validate_bool(pre_validate, 'pre_validate')
+        if pre_validate:
+            _validate_phase(new_phase, 'new_phase')
+        self._phase = numpy.mod(new_phase, 2 * numpy.pi)
+
+    def _validate(self) -> None:
+        """Validate attribute values."""
+        self._validate_abc()
+        self._validate_phase()
+
+    def _validate_frequencies(freq0: float | int, freq1: float | int) -> None:
+        """Validate the frequencies under their own strength and against each other."""
+        min_dev = 0.5 * self.symbol_rate  # Minimum deviation between the two freqs
+        validate_int_or_float(freq0, 'freq0')
+        validate_int_or_float(freq1, 'freq1')
+        freq_dev = abs(freq0 - freq1)
+        if freq_dev < min_dev:
+            raise ValueError(f'The deviation between "{freq0}" and "{freq1}" must be at '
+                             f'*least* "{min_dev}"')
+
+    def _validate_phase(self) -> None:
+        """Validate _phase attribute."""
+        _validate_phase(phase=self._phase, param_name='internal attribute _phase')
+
+def _validate_phase(phase: float, param_name: str) -> None:
+    """Validate phase, as a SPOT, on behalf of this module."""
+    upper_bound = 2 * math.pi  # Upper limit for self._phase
+    validate_float(phase, param_name)
+    if phase < 0:
+        raise ValueError(f'The {param_name} value may not be negative: {phase}')
+    if phase > upper_bound:
+        raise ValueError(f'The {param_name} value may not be greater than {upper_bound}: '
+                         f'{phase}')

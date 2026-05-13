d67892755423e39197b04b20ae9ca1ea7ff65e45
hark130 <hark130@yahoo.com>
2026-05-13 06:33:48 -0500

    GAIN-16: Added FSK2 maps to modem.constants
    Added an example comment and a common-use default mapping
    
    Changes to be committed:
    	modified:   gallant_input/modem/constants.py
    
    Changes not staged for commit:
    	modified:   devops/scripts/run_linters.sh
    	modified:   gallant_input/modem/modem.py
    
    Untracked files:
    	GAIN-16_bfsk_mod1_4800hz.iq
    	GAIN-16_bfsk_mod2_4800hz.iq
    	GAIN-16_fsk_mod1_4800hz.iq
    	GAIN-16_fsk_mod2_4800hz.iq
    	gallant_input/modem/fsk2.py

--- gallant_input/modem/constants.py
+++ gallant_input/modem/constants.py
@@ -9,6 +9,9 @@ from typing import Final
 # DEFAULT BITS --> SYMBOL MAPPINGS
 # Amplitude Modulation (AM)
 OOK_MAP: Final[dict[int, float]] = {0: 0.0, 1: 1.0}
+# Frequency Modulation (FM)
+# FSK2_MAP: Final[dict[int, float]] = {0: -baud_rate / 2, 1: baud_rate / 2}
+FSK2_MAP: Final[dict[int, float]] = {0: -115200 / 2, 1: 115200 / 2}  # Baud Rate: 115200
 # Phase Modulation (PM)
 BPSK_MAP: Final[dict[int, complex]] = {0: -1+0j, 1: 1+0j}
 QPSK_MAP: Final[dict[int, complex]] = {0b00: 1+1j, 0b01: -1+1j, 0b10: 1-1j, 0b11: -1-1j}

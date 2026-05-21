e6c4ad48be54eac7faeae7d9cc0bd8f71d490ae0
hark130 <hark130@yahoo.com>
2026-05-21 12:16:45 -0500

    GAIN-16: Refactored Modem() to use ModemConfig()
    Refactored Modem()'s ctor to "gently" extract ModemConfig() values into
    instance attributes.  Everything else should work as is.
    
    SPOILERS: The test code is still mad
    
    Changes to be committed:
    	modified:   gallant_input/modem/modem.py
    
    Changes not staged for commit:
    	modified:   gallant_input/modem/fsk2.py

--- gallant_input/modem/modem.py
+++ gallant_input/modem/modem.py
@@ -7,25 +7,26 @@ from abc import ABC, abstractmethod
 import numpy
 # Local Imports
 from gallant_input.modem.calc import calculate_sps
+from gallant_input.modem.modem_config import ModemConfig
 from gallant_input.validation import validate_bool, validate_pos_float_or_int, validate_pos_int
 
 
 class Modem(ABC):
     """Abstract base class (ABC) for modulation and demodulation."""
 
-    def __init__(self, sample_rate: float | int, symbol_rate: float | int, *args, **kwargs):
+    def __init__(self, config: ModemConfig):
         """Class ctor.
 
         Args:
             sample_rate: The sample rate of the capture in Hz.
             symbol_rate: The number of symbols-per-second (1 / symbol time).
         """
-        self.sample_rate = sample_rate  # Sample rate
-        self.symbol_rate = symbol_rate  # Symbol rate
-        self._parsed = False            # Input parsed
-        self._sps = 0                   # Samples per symbol
-        self._validated = False         # Validation status of attributes
-        super().__init__(*args, **kwargs)
+        self.sample_rate = None  # Sample rate
+        self.symbol_rate = None  # Symbol rate
+        self._parsed = False     # Input parsed
+        self._sps = 0            # Samples per symbol
+        self._validated = False  # Validation status of attributes
+        self._parse_abc_config(config=config)  # Gently update instance attributes from config
 
     @abstractmethod
     def modulate(self, bin_bytes: bytes) -> numpy.ndarray:
@@ -65,6 +66,16 @@ class Modem(ABC):
         self._sps = calculate_sps(self.sample_rate, self.symbol_rate)
         # Immediately validate it
         validate_pos_int(self._sps, 'internally calculated samples per symbol')
+
+    def _parse_abc_config(self, config: ModemConfig) -> None:
+        """Gently extract config values into instance attributes."""
+        try:
+            if isinstance(config, ModemConfig):
+                config.validate_content()
+                self.sample_rate = config.sample_rate
+                self.symbol_rate = config.symbol_rate
+        except (TypeError, ValueError):
+            pass  # Don't update or raise anything.  Subsequent method calls will catch it.
 
     def _validate_abc(self) -> None:
         """Validate attribute values in the ABC."""

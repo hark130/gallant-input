66f5fa00124d730fae33f4aecacb4ec91687683d
hark130 <hark130@yahoo.com>
2026-05-26 13:21:22 -0500

    GAIN-16: Big FSK2() refactor
    - Changed instance attributes to be public (because that's how they are on Modem) and updated methods
    - Changed config to optionally be OOKConfig (see: NOTE)
    - Refactored FSK2() to utilize the config instead of method arguments:
    	- modulate() only bits now
    - Refactored the parse and validate methods with an optional keyword: demod
    - Updated class validation to validate the FSK2-portion of the config
    
    Test cases passing locally for valid reasons.
    
    NOTE: I may have gotten things to work as need with the FSK2 ctor config argument as
    a FSK2Config | OOKConfig... but I don't like it.  It's not inuitive or obvious why.
    I decided to refactor the behavior of FSK2Config to make the parsing/validate
    functionality ignore the "optional" values during "demodulate" usage.
    Meanwhile, I'm committing/pushing this milestone.
    
    Changes to be committed:
    	modified:   gallant_input/modem/fsk2.py
    
    Changes not staged for commit:
    	modified:   gallant_input/modem/fsk2_config.py
    	modified:   gallant_input/modem/modem.py
    	modified:   gallant_input/modem/ook.py
    	modified:   test/unit_test/test_modem/test_fsk2/test_modem_fsk2.py
    	modified:   test/unit_test/test_modem/test_fsk2/test_modem_fsk2_demodulate.py
    	modified:   test/unit_test/test_modem/test_fsk2/test_modem_fsk2_modulate.py

--- gallant_input/modem/fsk2.py
+++ gallant_input/modem/fsk2.py
@@ -8,8 +8,9 @@ from gallant_input.codec import convert_ascii_bin_bytes_to_bits, stringify_ndarr
 from gallant_input.modem.calc import reshape_to_symbols
 from gallant_input.modem.fsk2_config import FSK2Config
 from gallant_input.modem.modem import Modem
+from gallant_input.modem.ook_config import OOKConfig
 from gallant_input.validation import (validate_binary_bytes, validate_bool, validate_int_or_float,
-                                      validate_ndarray, validate_phase)
+                                      validate_ndarray, validate_phase, validate_type)
 
 
 class FSK2(Modem):
@@ -17,25 +18,25 @@ class FSK2(Modem):
 
     # CORE METHODS
 
-    def __init__(self, config: FSK2Config):
-        """Class ctor."""
-        self._freq0 = None        # The 'off' frequency baseband deviation.
-        self._freq1 = None        # The 'on' frequency baseband deviation.
+    def __init__(self, config: FSK2Config | OOKConfig):
+        """Class ctor.
+
+        Args:
+            config: Necessary configuration.  Modulation requires FSK2Config but demodulation
+                only requires a OOKConfig.
+        """
+        self.freq0 = None         # The 'off' frequency baseband deviation.
+        self.freq1 = None         # The 'on' frequency baseband deviation.
         self._phase = float(0.0)  # Phase state
-        self._parse_fsk2_config(config=config)  # Gently update instance attributes from config
         super().__init__(config=config)
 
     # ABSTRACT METHODS
 
-    def modulate(self, bin_bytes: bytes, freq0: float | int, freq1: float | int,
-                 phase: float | None = None) -> numpy.ndarray:
+    def modulate(self, bin_bytes: bytes) -> numpy.ndarray:
         """MOdulate binary data.
 
         Args:
             bin_bytes: A bytes object containing binary to modulate.
-            freq0: The 'off' frequency baseband deviation.
-            freq1: The 'on' frequency baseband deviation.
-            phase: [OPTIONAL] Override the internal phase continuity.
 
         Returns:
             The modulated binary data.
@@ -54,14 +55,11 @@ class FSK2(Modem):
 
         # VALIDATION
         self.parse()  # Validate and parse
-        self._validate_frequencies(freq0=freq0, freq1=freq1)
         _validate_bin_bytes(bin_bytes=bin_bytes)
-        bits = convert_ascii_bin_bytes_to_bits(bin_bytes)
-        if phase is not None:
-            self._update_phase(phase, pre_validate=True)  # Check it prior to final update
 
         # MODULATE IT
-        freqs = numpy.where(bits == 0, freq0, freq1)
+        bits = convert_ascii_bin_bytes_to_bits(bin_bytes)
+        freqs = numpy.where(bits == 0, self.freq0, self.freq1)
         for freq in freqs:
             phase_inc = 2 * numpy.pi * freq / self.sample_rate
             phi = self._phase + phase_inc * numpy.arange(self._sps)
@@ -92,7 +90,7 @@ class FSK2(Modem):
         bits = None       # An array of bits extracted from samples
 
         # VALIDATION
-        self.parse()  # Validate and parse
+        self.parse(demod=True)  # Validate and parse
         validate_ndarray(array=samples, array_name='samples', can_be_empty=False, num_dim=1,
                          must_be_complex=True)
 
@@ -115,26 +113,34 @@ class FSK2(Modem):
         self._validate_phase()  # Check it again just to be sure
         return self._phase
 
-# I'm not (yet) comfortable moving this code up to Modem() because I suspect I'll have to
-# special-case something in a future child class.
-# pylint: disable = duplicate-code
-    def parse(self) -> None:
+    def parse(self, demod: bool = False) -> None:
         """Validate, parse and update attributes once.
+
+        Args:
+            demod: [OPTIONAL] Controls internal parsing/validation.  If True, the ctor config
+                is parsed as a OOKConfig, instead of a FSK2Config, because demodulation doesn't
+                require FSK2Config-specific values.
 
         Raises:
             TypeError: Bad data type.
-            ValueError: Badd value.
+            ValueError: Bad value.
         """
         # VALIDATION
         validate_bool(self._parsed, 'internal attribute _parsed')
-        self.validate()
+        validate_bool(demod, 'demod')
+        self.validate(demod=demod)
         # PARSE IT
         if not self._parsed:
-            self._parse()
+            self._parse(demod=demod)
             self._parsed = True
 
-    def validate(self) -> None:
+    def validate(self, demod: bool = False) -> None:
         """Validate attribute values once.
+
+        Args:
+            demod: [OPTIONAL] Controls internal parsing/validation.  If True, the ctor config
+                is parsed as a OOKConfig, instead of a FSK2Config, because demodulation doesn't
+                require FSK2Config-specific values.
 
         Raises:
             TypeError: Bad data type.
@@ -142,28 +148,26 @@ class FSK2(Modem):
         """
         # VALIDATION
         validate_bool(self._validated, 'internal attribute _validated')
+        validate_bool(demod, 'demod')
         if not self._validated:
-            self._validate()
+            self._validate(demod=demod)
             self._validated = True
-# pylint: enable = duplicate-code
 
     # PRIVATE METHODS
 
-    def _parse(self) -> None:
+    def _parse(self, demod: bool) -> None:
         """Parse user input."""
         self._parse_abc()
+        if not demod:
+            self._parse_fsk2_config()  # Get the rest of the data from the child object
 
-    def _parse_fsk2_config(self, config: FSK2Config) -> None:
+    def _parse_fsk2_config(self) -> None:
         """Gently extract config values into instance attributes."""
-        try:
-            if isinstance(config, FSK2Config):
-                config.validate_content()
-                self._freq0 = config.freq0
-                self._freq1 = config.freq1
-                if config.phase is not None:
-                    self._phase = config.phase
-        except (TypeError, ValueError):
-            pass  # Don't update or raise anything.  Subsequent method calls will catch it.
+        if isinstance(self._config, FSK2Config):
+            self.freq0 = self._config.freq0
+            self.freq1 = self._config.freq1
+            if self._config.phase is not None:
+                self._update_phase(self._config.phase, pre_validate=True)  # Check it first
 
     def _update_phase(self, new_phase: float, pre_validate: bool = False) -> None:
         """Trim and update the phase attribute."""
@@ -172,14 +176,17 @@ class FSK2(Modem):
             validate_phase(new_phase, 'new_phase')
         self._phase = numpy.mod(new_phase, 2 * numpy.pi)
 
-    def _validate(self) -> None:
+    def _validate(self, demod: bool) -> None:
         """Validate attribute values."""
         self._validate_abc()
-        self._validate_phase()
+        if not demod:
+            self._validate_fsk2_config()
+            self._validate_phase()
 
-    def _validate_frequencies(self, freq0: float | int, freq1: float | int) -> None:
+    def _validate_frequencies(self, symbol_rate: float | int,
+                              freq0: float | int, freq1: float | int) -> None:
         """Validate the frequencies under their own strength and against each other."""
-        min_dev = 0.5 * self.symbol_rate  # Minimum deviation between the two freqs
+        min_dev = 0.5 * symbol_rate  # Minimum deviation between the two freqs
         validate_int_or_float(freq0, 'freq0')
         validate_int_or_float(freq1, 'freq1')
         freq_dev = abs(freq0 - freq1)
@@ -187,13 +194,20 @@ class FSK2(Modem):
             raise ValueError(f'The deviation between "{freq0}" and "{freq1}" must be at '
                              f'*least* "{min_dev}"')
 
+    def _validate_fsk2_config(self) -> None:
+        """Validate the FSK2Config object."""
+        validate_type(self._config, 'config', FSK2Config)
+        self._config.validate_content()
+        self._validate_frequencies(symbol_rate=self._config.symbol_rate,
+                                   freq0=self._config.freq0, freq1=self._config.freq1)
+
     def _validate_phase(self) -> None:
         """Validate _phase attribute."""
-        validate_phase(phase=self._phase, param_name='internal attribute _phase')
+        validate_phase(phase=self._phase, param_name='internal phase attribute')
 
 
 def _validate_bin_bytes(bin_bytes: bytes) -> None:
-    """Validate bin bytes prior to conversion."""
+    """Validate bin bytes, as non-empty, prior to conversion."""
     validate_binary_bytes(bin_bytes, 'bin_bytes', exact_len=None)
     if not bin_bytes:
         raise ValueError('The "bin_bytes" argument may not be empty')

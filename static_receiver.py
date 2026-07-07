51a52efb7276a7c2c7a9d1a2380157ea35cec29b
hark130 <hark130@yahoo.com>
2026-07-07 10:47:27 -0500

    GAIN-17: Static receiver milestone
    GOOD NEWS: It still works (post-TGUC refactor)
    BAD NEWS: It looks like trash in here:
    	- Stale code
    	- Old function calls
    	- Linter violations
    
    Interim commit...
    
    TGUC:
    ✓. Extricate
    2. Lint
    3. Move static receiver into dedicated package: rxtx
    4. Verify static receiver still works
    5. Fix(?) the test cases
    6. Start a template static receiver in rxtx
    7. Test it on another FSK capture
    8. Merge into GAIN-16
    9. Continue GAIN-16
    
    Changes to be committed:
    	modified:   static_receiver.py
    
    Changes not staged for commit:
    	modified:   gallant_input/modem/fsk2.py
    
    Untracked files:
    	gallant_input/synch/

--- static_receiver.py
+++ static_receiver.py
@@ -1,18 +1,20 @@
 """Born from my shmelstone_receiver and refactored to escape CPT Fox Time (CFT).
 
 Example Usage:
-    python static_receiver.py ./data/shmelstone_fake_ski1_c0hz_s2p4M_b2p4k.sigmf-data  # Injected
-    python static_receiver.py ./data/shmelstone_fake_tasha1_c0hz_s2p4M_b2p4k.sigmf-data  # Injected
-    python static_receiver.py ./data/shmelstone_filt_cap_c912m_s2m.sigmf-data  # Test message
+    # Working
     python static_receiver.py ./data/shmelstone_filt_cap_c912m_s2p4m_hark1.sigmf-data
     python static_receiver.py ./data/shmelstone_filt_cap_c912m_s2p4m_msg1_max.sigmf-data
     python static_receiver.py ./data/shmelstone_filt_cap_c912m_s2p4m_msg2_long.sigmf-data
     python static_receiver.py ./data/shmelstone_filt_cap_c912m_s2p4m_msg3_not_short.sigmf-data
-
+    # Non-standard
+    python static_receiver.py ./data/shmelstone_fake_ski1_c0hz_s2p4M_b2p4k.sigmf-data  # Injected
+    python static_receiver.py ./data/shmelstone_fake_tasha1_c0hz_s2p4M_b2p4k.sigmf-data  # Injected
+    python static_receiver.py ./data/shmelstone_filt_cap_c912m_s2m.sigmf-data  # Test message
 """
 
 # Standard Imports
 from pathlib import Path
+from scipy import signal
 from typing import Any, Final
 import numpy  # CFT
 import sys
@@ -20,9 +22,9 @@ import sys
 import matplotlib.pyplot as plt
 # Local Imports
 from gallant_input.analyze import analyze_spectrum
-from gallant_input.codec import upsample
+from gallant_input.codec import convert_ascii_bin_bytes_to_bits, upsample
 from gallant_input.constants import SIGMF_DATA_FILE_EXT, SIGMF_META_FILE_EXT
-from gallant_input.converters import convert_bin_bytes_to_ascii, convert_bin_bytes_to_int
+from gallant_input.converters import convert_bin_bytes_to_ascii, convert_bin_bytes_to_int, convert_bin_bytes_to_ndarray
 from gallant_input.gain_sigmf.sigmfmetaparser import SigMFMetaParser
 from gallant_input.io import read_samples
 from gallant_input.modem.calc import calculate_sps
@@ -30,47 +32,20 @@ from gallant_input.modem.fsk2 import FSK2
 from gallant_input.modem.fsk2_config import FSK2Config
 from gallant_input.modscheme import ModScheme
 from gallant_input.plot import plot_constellation, plot_time_domain, plot_spectrum, plot_symbol_boundaries
-from gallant_input.signal import compute_basic_fft, detect_signal, downconvert_signal
+from gallant_input.signal import compute_basic_fft, decimate_samples, detect_signal, downconvert_signal, squelch_signal
+from gallant_input.synch.frame import correlate_it
+from gallant_input.synch.timing import recover_clock_mm
+from gallant_input.timing import estimate_symbol_clock
 
 DEF_SAMP_RATE: Final[int] = 2400000
 DEF_SYMB_RATE: Final[int] = 2400
+DEF_PREAMBLE: Final[bytes] = b'01' * 32
 DEF_SYNCWORD: Final[bytes] = b'11010011100100011101001110010001'  # 0xD391 0xD391
+DEF_PREAMWRD: Final[bytes] = DEF_PREAMBLE[-8:] + DEF_SYNCWORD  # Some preamble + sw
 
 
-def convert_bin_bytes_to_ndarray(bin_bytes: bytes, bipolar: bool = True) -> numpy.ndarray:
-    """Convert binary bytes to an ndarray."""
-    bits = (numpy.frombuffer(bin_bytes, dtype=numpy.uint8) == ord('1')).astype(numpy.int8)
-
-    if bipolar is True:
-        return bits * 2 - 1
-
-    return bits
-
-
-def correlate_it(thing1: Any, thing2: Any) -> int:
-    """Correlate thing2 with thing1 returning a thing1 index of highest correlation."""
-    # LOCAL VARIABLES
-    arr1 = thing1
-    arr2 = thing2
-    corr = None    # Correlation array
-    index = 0      # Index of highest correlation into thing1 (AKA arr1)
-
-    # SETUP
-    if isinstance(arr1, bytes):
-        arr1 = convert_bin_bytes_to_ndarray(arr1)
-    if isinstance(arr2, bytes):
-        arr2 = convert_bin_bytes_to_ndarray(arr2)
-
-    # CORRELATE IT
-    corr = numpy.correlate(arr1, arr2, mode='valid')
-    index = int(corr.argmax())
-
-    # DONE
-    return index
-
-
-def demod_to_bytes(samples: numpy.ndarray, sample_rate: float | int,
-                   symbol_rate: float | int) -> bytes:
+def demod_to_bytes_full(samples: numpy.ndarray, sample_rate: float | int,
+                        symbol_rate: float | int) -> bytes:
     """Demodulate a signal to binary bytes."""
     # LOCAL VARIABLES
     config = FSK2Config(sample_rate=sample_rate, symbol_rate=symbol_rate, freq0=0, freq1=0)
@@ -81,6 +56,23 @@ def demod_to_bytes(samples: numpy.ndarray, sample_rate: float | int,
     config.set_demod()  # Demodulate only
     modem = FSK2(config=config)
     bin_bytes = modem.demodulate(samples=samples)
+
+    # DONE
+    return bin_bytes
+
+
+def demod_to_bytes_partial(real_wave: numpy.ndarray, sample_rate: float | int,
+                           symbol_rate: float | int) -> bytes:
+    """Demodulate a real waveform to binary bytes."""
+    # LOCAL VARIABLES
+    config = FSK2Config(sample_rate=sample_rate, symbol_rate=symbol_rate, freq0=0, freq1=0)
+    modem = None     # The demodulator class
+    bin_bytes = b''  # The demodulated binary
+
+    # DEMODULATE IT
+    config.set_demod()  # Demodulate only
+    modem = FSK2(config=config)
+    bin_bytes = modem.demodulate_to_bytes(real_wave=real_wave)
 
     # DONE
     return bin_bytes
@@ -125,22 +117,14 @@ def get_filename() -> Path:
 def parse_payload(payload: bytes) -> None:
     """Parse and print the payload."""
     # PARSE IT
-    msg_len = convert_bin_bytes_to_int(payload[:8])                          # Remaining payload len
-    msg_type = convert_bin_bytes_to_int(payload[8:16])                       # Message type
-    user_len = convert_bin_bytes_to_int(payload[16:24])                      # Len of the username
-    user_name = convert_bin_bytes_to_ascii(payload[24:24 + (user_len * 8)])  # Sender username
-    message = convert_bin_bytes_to_ascii(payload[24 + (user_len * 8):])      # Sender's message
+    msg_len = convert_bin_bytes_to_int(payload[:8])                                       # Length
+    msg_type = convert_bin_bytes_to_int(payload[8:16])                                    # Msg type
+    user_len = convert_bin_bytes_to_int(payload[16:24])                                   # Name len
+    user_name = convert_bin_bytes_to_ascii(payload[24:24 + (user_len * 8)])               # Username
+    message = convert_bin_bytes_to_ascii(payload[24 + (user_len * 8):(msg_len + 1) * 8])  # Message
 
     # PRINT IT
     print(f'User "{user_name}" sent message: {message}')
-
-
-def squelch_it(samples: numpy.ndarray, threshold: float | int) -> numpy.ndarray:
-    """Squelch the samples given a threshold."""
-    mag = numpy.abs(samples)        # Magnitude
-    mag_db = 10 * numpy.log10(mag)  # Convert magnitude to power
-    squelched = samples[mag_db > threshold]
-    return squelched
 
 
 def main() -> None:
@@ -160,15 +144,27 @@ def main() -> None:
         waveform = None                   # Demodulated real waveform
         index = 0                         # Correlated index
         new_binary = b''                  # Frame synch'd binary
+        decimate = 20                     # Decimation
 
         # DO IT
         # Fetch SigMF metadata (if available)
         if filepath.suffix.lower() == f'.{SIGMF_DATA_FILE_EXT}'.lower():
             sig_meta_parser = SigMFMetaParser(filepath.with_suffix(f'.{SIGMF_META_FILE_EXT}'))
             sample_rate = sig_meta_parser.get_sample_rate()
+        # Decimate!
+        samples = decimate_samples(samples=samples, decimate=decimate)
+        sample_rate = sample_rate / decimate
         sps = calculate_sps(sample_rate=sample_rate, symbol_rate=symbol_rate)  # Calc the sps
+        # print(f'SPS: {sps}')  # DEBUGGING
+        # print(f'SAMPLE RATE: {sample_rate}')  # DEBUGGING
         # Squelch!
-        samples = squelch_it(samples=samples, threshold=-30)
+        # samples = squelch_signal(signal=samples, threshold=-50)
+        # samples = squelch_signal(signal=samples, threshold=-49)
+        samples = squelch_signal(signal=samples, threshold=-48)
+        # samples = squelch_signal(signal=samples, threshold=-47)
+        # samples = squelch_signal(signal=samples, threshold=-45)
+        # samples = squelch_signal(signal=samples, threshold=-30)
+        # samples = squelch_signal(signal=samples, threshold=-15)
         # Analyze Spectrum
         spect_analysis = analyze_spectrum(samples, sample_rate=sample_rate, max_peaks=2)
         # print(spect_analysis)  # DEBUGGING
@@ -183,28 +179,94 @@ def main() -> None:
         else:
             translated = samples
         #     plot_spectrum(translated, samp_rate=sample_rate, title='Original Freq vs. Mag')
+        # est_sps = estimate_symbol_clock(translated, sps)
+        # print(f'SPS CALC "{sps}" ESTIMATED "{est_sps}"')  # DEBUGGING
+        # sps = est_sps  # DEBUGGING
+        waveform = demod_to_waveform(samples=translated, sample_rate=sample_rate,
+                                     symbol_rate=symbol_rate)  # Used by 2 & 3
+        # correlate_this = upsample(convert_ascii_bin_bytes_to_bits(DEF_PREAMBLE), sps)  # Preamble
+        # correlate_this = upsample(convert_ascii_bin_bytes_to_bits(DEF_PREAMWRD), sps)  # Pream + sw
+        correlate_this = upsample(convert_ascii_bin_bytes_to_bits(DEF_SYNCWORD), sps)  # Syncword (used by 3 & 4)
 
         # DEMOD
+        # Attempt 6 - Time Sync w/ Interpolation
+
+
+        # Attempt 5 - M&M
+        # print(f'WAVEFORM LEN: {len(waveform)}')  # DEBUGGING
+        # plot_symbol_boundaries(waveform, sps)
+        # timing = recover_symbol_centers(waveform, sps, 0.1)
+        # mm_wave = recover_clock_mm(waveform, sps, interp=None)  # Do not interpolate
+        mm_wave = recover_clock_mm(waveform, sps, interp=16)  # Interpolate to better find boundaries
+        # binary = demod_to_bytes_partial(real_wave=mm_wave, sample_rate=sample_rate,
+        #                                 symbol_rate=symbol_rate)
+        threshold = numpy.median(mm_wave)
+        bits = (mm_wave > threshold).astype(numpy.uint8)
+        binary = b''.join(str(bit).encode() for bit in bits)
+
+        # Attempt 4 - Timing recover
+        # timing = recover_symbol_centers(waveform, sps, 1)
+        # print(f'TIMING SYMBOLS: {timing.symbols}')  # DEBUGGING
+        # # binary = demod_to_bytes_partial(real_wave=timing.symbols, sample_rate=sample_rate,
+        # #                                 symbol_rate=symbol_rate)
+        # threshold = numpy.median(timing.symbols)
+        # bits = (timing.symbols > threshold).astype(numpy.uint8)
+        # # print(f'BITS: {bits}')  # DEBUGGING
+        # binary = b''.join(str(bit).encode() for bit in bits)
+        # print(f'BINARY: {binary}')
+        # parse_payload(binary)
+        # # Plot
+        # plt.plot(waveform)
+        # for c in timing.predicted_centers:
+        #     plt.axvline(c, linestyle=":", color='red')
+        # for c in timing.centers:
+        #     plt.axvline(c, color='purple')
+        # plt.show()
+
+        # Attempt 3 - Correlate on the real waveform
+        # print(f'SOME PREAMBLE + SYNCWORD (len {len(DEF_PREAMWRD)}): {DEF_PREAMWRD}')  # DEBUGGING
+        # sps = est_sps  # DEBUGGING
+        # index = correlate_it(waveform, correlate_this)  # Used by 3 & 4
+        # # print(f'INDEX: {index} (OF {len(waveform)} SAMPLES)')  # DEBUGGING
+        # waveform = waveform[index + len(correlate_this):]  # Index past the preamble
+        # waveform = waveform[index + len(correlate_this) + int(sps/3):]  # WINGIN' IT... EE-style
+        # # print(f'INDEX: {index} (OF {len(waveform)} SAMPLES)')  # DEBUGGING
+        # plot_symbol_boundaries(waveform, sps)
+        # binary = demod_to_bytes_partial(real_wave=waveform, sample_rate=sample_rate,
+        #                                 symbol_rate=symbol_rate)
+        # print(f'BINARY: {binary}')  # DEBUGGING
+        # parse_payload(binary)
+        # BRUTE FORCE IT
+        # wave_copy = None
+        # for offset in range(0, index * 2, 1000):
+        #     try:
+        #         wave_copy = waveform[offset + len(correlate_this):]  # Index past the preamble
+        #         binary = demod_to_bytes_partial(real_wave=wave_copy, sample_rate=sample_rate,
+        #                                         symbol_rate=symbol_rate)
+        #         print(f'BINARY (OFFSET {offset} INDEX {index}): {binary}')  # DEBUGGING
+        #         parse_payload(binary)
+        #     except Exception as err:
+        #         print(f'OFFSET {offset} INDEX {index} RAISED {err}')
+
         # Attempt 1 - Frame Sync
-        binary = demod_to_bytes(samples=translated, sample_rate=sample_rate,
-                                symbol_rate=symbol_rate)
-        # # print(f'BINARY: {binary}')  # DEBUGGING
+        # binary = demod_to_bytes_full(samples=translated, sample_rate=sample_rate,
+        #                              symbol_rate=symbol_rate)
+        # print(f'BINARY: {binary}')  # DEBUGGING
         index = correlate_it(binary, DEF_SYNCWORD)
-        print(f'INDEX: {index}')  # DEBUGGING
-        # new_binary = binary[index + len(DEF_SYNCWORD):]
-        # # print(f'FRAME SYNC BINARY: {new_binary}')  # DEBUGGING
-        # parse_payload(new_binary)
-        # Attemp 2 - Waveform Sync
-        waveform = demod_to_waveform(samples=translated, sample_rate=sample_rate,
-                                     symbol_rate=symbol_rate)
-        plot_symbol_boundaries(waveform, sps)
-        plot_time_domain(samples=waveform, samp_rate=sample_rate, title='Demod Real Waveform')
+        # print(f'INDEX: {index}')  # DEBUGGING
+        new_binary = binary[index + len(DEF_SYNCWORD):]
+        # print(f'FRAME SYNC BINARY: {new_binary}')  # DEBUGGING
+        parse_payload(new_binary)
+
+        # Attempt 2 - Waveform Sync
+        # plot_symbol_boundaries(waveform, sps)
+        # plot_time_domain(samples=waveform, samp_rate=sample_rate, title='Demod Real Waveform')
 
         # TO DO: DON'T DO NOW...
-        #   - Comment out/remove the janky timing attempt inside FSK2
-        #   - Consider rolling back to the original FSK2.demod() attempt
-        #   - Try to correlate on the real demod samples instead of the binary
-        #   - Try the half-over coarse timing sync (on the real demod samples)
+        #   - Comment out/remove the janky timing attempt inside FSK2  # DONE (by shunting to demod_to_waveform)
+        #   - Consider rolling back to the original FSK2.demod() attempt  # Maybe?
+        #   - Try to correlate on the real demod samples instead of the binary  # NEXT!
+        #   - Try the half-over coarse timing sync (on the real demod samples)  # ...if correlating on real samples doesn't work
     except Exception as err:
         print(f'Execution failed with: {repr(err)}', file=sys.stderr, flush=True)
         raise err from err  # DEBUGGING

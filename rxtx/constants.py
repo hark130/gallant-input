c970c13873b3a6d16e05d2eb4ba86287324c4aaf
hark130 <hark130@yahoo.com>
2026-07-08 12:47:54 -0500

    GAIN-17: Initial commit of rxtx constants
    So far, it's just used by the arg parser.
    
    Changes to be committed:
    	new file:   rxtx/constants.py
    
    Untracked files:
    	rxtx/static_rx_shmelstone2.py
    	rxtx/static_rx_template.py

---
+++ rxtx/constants.py
@@ -0,0 +1,23 @@
+"""Defines SPOT constants on behalf of the package."""
+
+# Standard Imports
+from typing import Final
+# Third Party Imports
+# Local Imports
+
+
+##############
+### rxtx.* ###
+##############
+# Temporary directory constants
+PKG_SHORT_TITLE: Final[str] = 'rxtx'  # Receive/Transmit
+
+
+#######################
+### rxtx.arg_parser ###
+#######################
+# Arguments
+RXTX_CLI_ARG_BAUD_RATE: Final[str] = 'baud'      # Symbol rate
+RXTX_CLI_ARG_DEBUG: Final[str] = 'debug'         # Debug behavior
+RXTX_CLI_ARG_FILENAME: Final[str] = 'filename'   # Data filename
+RXTX_CLI_ARG_SAMP_RATE: Final[str] = 'samprate'  # Sample rate

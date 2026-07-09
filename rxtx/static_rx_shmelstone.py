98a2e09353ebf1c334d9053f927228879a7fd5f0
hark130 <hark130@yahoo.com>
2026-07-09 06:15:16 -0500

    GAIN-17: Renamed shmelstone static receiver
    Pylint was freaking out over all the duplicate code.
    The new one is the good one.
    
    Changes to be committed:
    	modified:   rxtx/static_rx_shmelstone.py
    	deleted:    rxtx/static_rx_shmelstone2.py

--- rxtx/static_rx_shmelstone.py
+++ rxtx/static_rx_shmelstone.py
@@ -1,10 +1,15 @@
-"""Born from my shmelstone_receiver and refactored to escape CPT Fox Time (CFT).
+"""Reimplemented rxtx.static_rx_shmelstone as rxtx.static_rx_template.
 
 Example Usage:
-    python -m rxtx.static_rx_shmelstone ./data/shmelstone_filt_cap_c912m_s2p4m_hark1.sigmf-data
-    python -m rxtx.static_rx_shmelstone ./data/shmelstone_filt_cap_c912m_s2p4m_msg1_max.sigmf-data
-    python -m rxtx.static_rx_shmelstone ./data/shmelstone_filt_cap_c912m_s2p4m_msg2_long.sigmf-data
-    python -m rxtx.static_rx_shmelstone ./data/shmelstone_filt_cap_c912m_s2p4m_msg3_med.sigmf-data
+    # Windows
+    python -m rxtx.static_rx_shmelstone2 --baud 2400 `
+        --filename ./data/shmelstone_filt_cap_c912m_s2p4m_hark1.sigmf-data
+    python -m rxtx.static_rx_shmelstone2 --baud 2400 `
+        --filename ./data/shmelstone_filt_cap_c912m_s2p4m_msg1_max.sigmf-data
+    python -m rxtx.static_rx_shmelstone2 --baud 2400 `
+        --filename ./data/shmelstone_filt_cap_c912m_s2p4m_msg2_long.sigmf-data
+    python -m rxtx.static_rx_shmelstone2 --baud 2400 `
+        --filename ./data/shmelstone_filt_cap_c912m_s2p4m_msg3_med.sigmf-data
 """
 
 # Standard Imports
@@ -22,11 +27,16 @@ from gallant_input.io import read_samples
 from gallant_input.modem.calc import calculate_sps
 from gallant_input.modem.fsk2 import FSK2
 from gallant_input.modem.fsk2_config import FSK2Config
+from gallant_input.modem.modem import Modem
+from gallant_input.modem.modem_config import ModemConfig
 from gallant_input.modscheme import ModScheme
 from gallant_input.signal import (decimate_samples, detect_signal, downconvert_signal,
                                   squelch_signal)
 from gallant_input.synch.frame import correlate_it
 from gallant_input.synch.timing import recover_clock_mm
+from gallant_input.validation import validate_file, validate_type
+from rxtx.arg_parser import parse_args, print_help
+from rxtx.argvals import ArgVals
 
 DEF_SAMP_RATE: Final[int] = 2400000
 DEF_SYMB_RATE: Final[int] = 2400
@@ -35,73 +45,112 @@ DEF_SYNCWORD: Final[bytes] = b'11010011100100011101001110010001'  # 0xD391 0xD39
 DEF_PREAMWRD: Final[bytes] = DEF_PREAMBLE[-8:] + DEF_SYNCWORD  # Some preamble + sw
 
 
-def demod_to_bytes_full(samples: numpy.ndarray, sample_rate: float | int,
-                        symbol_rate: float | int) -> bytes:
-    """Demodulate a signal to binary bytes."""
-    # LOCAL VARIABLES
+def build_modem(config: ModemConfig) -> Modem:
+    """Build a Modem child class object."""
+    modem_obj = FSK2(config=config)
+    return modem_obj
+
+
+def build_modem_config(sample_rate: float | int, symbol_rate: float | int, **kwargs) -> ModemConfig:
+    """Build a ModemConfig child class object."""
     config = FSK2Config(sample_rate=sample_rate, symbol_rate=symbol_rate, freq0=0, freq1=0)
-    modem = None     # The demodulator class
-    bin_bytes = b''  # The demodulated binary
+    return config
+
+
+def decide_symbols(symbol_metrics: numpy.ndarray, sample_rate: float | int,
+                   symbol_rate: float | int) -> bytes:
+    """Demodulate recovered symbols to binary bytes (Demod Step 3)."""
+    # LOCAL VARIABLES
+    config = build_modem_config(sample_rate=sample_rate, symbol_rate=symbol_rate)  # Config obj
+    modem = build_modem(config=config)                                             # Modem obj
+    bin_bytes = b''                                                                # Binary output
 
     # DEMODULATE IT
-    config.set_demod()  # Demodulate only
-    modem = FSK2(config=config)
-    bin_bytes = modem.demodulate(samples=samples)
+    bin_bytes = modem.decide_symbols(symbol_metrics=symbol_metrics)
 
     # DONE
     return bin_bytes
 
 
-def demod_to_bytes_partial(real_wave: numpy.ndarray, sample_rate: float | int,
-                           symbol_rate: float | int) -> bytes:
-    """Demodulate a real waveform to binary bytes."""
+def demod_to_metric(samples: numpy.ndarray, sample_rate: float | int,
+                    symbol_rate: float | int) -> numpy.ndarray:
+    """Demodulate a signal to a continuous-valued symbol metric (Demod Step 1)."""
     # LOCAL VARIABLES
-    config = FSK2Config(sample_rate=sample_rate, symbol_rate=symbol_rate, freq0=0, freq1=0)
-    modem = None     # The demodulator class
-    bin_bytes = b''  # The demodulated binary
+    config = build_modem_config(sample_rate=sample_rate, symbol_rate=symbol_rate)  # Config obj
+    modem = build_modem(config=config)                                             # Modem obj
+    symbol_metrics = None                                                          # Symbol metrics
 
     # DEMODULATE IT
-    config.set_demod()  # Demodulate only
-    modem = FSK2(config=config)
-    bin_bytes = modem.demodulate_to_bytes(real_wave=real_wave)
+    symbol_metrics = modem.demodulate_to_metric(samples=samples)
+
+    # DONE
+    return symbol_metrics
+
+
+def demodulate(samples: numpy.ndarray, sample_rate: float | int,
+               symbol_rate: float | int) -> bytes:
+    """Completeely demodulate a signal to binary bytes (Demod Steps 1 - 3)."""
+    # LOCAL VARIABLES
+    config = build_modem_config(sample_rate=sample_rate, symbol_rate=symbol_rate)  # Config obj
+    modem = build_modem(config=config)                                             # Modem obj
+    bin_bytes = b''                                                                # Binary output
+
+    # DEMODULATE IT
+    bin_bytes = modem.demodulate(samples=samples)  # Full demod
 
     # DONE
     return bin_bytes
 
 
-def demod_to_waveform(samples: numpy.ndarray, sample_rate: float | int,
-                      symbol_rate: float | int) -> numpy.ndarray:
-    """Demodulate a signal to a real waveform."""
+def get_filename(arg_vals: ArgVals) -> Path:
+    """Form the filename argument into a Path object."""
     # LOCAL VARIABLES
-    config = FSK2Config(sample_rate=sample_rate, symbol_rate=symbol_rate, freq0=0, freq1=0)
-    modem = None     # The demodulator class
-    waveform = None  # The demodulated real waveform
+    filename = None  # CLI argument filename as a Path obj
 
-    # DEMODULATE IT
-    config.set_demod()  # Demodulate only
-    modem = FSK2(config=config)
-    waveform = modem.demodulate_to_samples(samples=samples)
-
-    # DONE
-    return waveform
-
-
-def get_filename() -> Path:
-    """In lieu of an actual argument parser, just get the filename."""
-    # LOCAL VARIABLES
-    arg_val = None   # argv[1]
-    filename = None  # argv[1] as a Path object
+    # VALIDATION
+    validate_type(arg_vals, 'arg_vals', ArgVals)
 
     # GET IT
     try:
-        arg_val = sys.argv[1]
-    except IndexError as err:
-        print_help()
+        filename = Path(arg_vals.filename)
+    except (ValueError, TypeError) as err:
         raise err from err
-    filename = Path(arg_val)
 
     # DONE
     return filename
+
+
+def get_sample_rate(arg_vals: ArgVals, filepath: Path) -> float | int:
+    """Get the sample rate, in order of priority, from: CLI args, filepath SigMF metadata."""
+    # LOCAL VARIABLES
+    sample_rate = None                                                # Sample rate
+    meta_path = None                                                  # SigMF metadata filepath
+    except_msg = 'Unable to fetch the sample rate from the CLI args'  # Base Exception message
+
+    # VALIDATION
+    validate_type(arg_vals, 'arg_vals', ArgVals)
+    validate_file(filepath, 'filepath', must_exist=True)
+
+    # GET IT
+    # 1. CLI args?
+    sample_rate = arg_vals.sample_rate
+    # 2. SigMF?
+    if sample_rate is None:
+        # Fetch SigMF metadata (if available)
+        if filepath.suffix.lower() == f'.{SIGMF_META_FILE_EXT}'.lower():
+            meta_path = filepath
+        elif filepath.suffix.lower() == f'.{SIGMF_DATA_FILE_EXT}'.lower():
+            meta_path = filepath.with_suffix(f'.{SIGMF_META_FILE_EXT}')
+        if meta_path:
+            sig_meta_parser = SigMFMetaParser(meta_path)
+            sample_rate = sig_meta_parser.get_sample_rate()
+
+    # DONE
+    if sample_rate is None:
+        if meta_path is not None:
+            except_msg = except_msg + f' or "{meta_path.absolute()}"'
+        raise RuntimeError(except_msg)
+    return sample_rate
 
 
 def parse_payload(payload: bytes) -> None:
@@ -117,85 +166,83 @@ def parse_payload(payload: bytes) -> None:
     print(f'User "{user_name}" sent message type {msg_type}: {message}')
 
 
-def print_help() -> None:
-    """Print a help message."""
-    # command_name [options/flags] <required_argument>
-    print(f'\n\nUSAGE: {sys.argv[0]} <SIGMF FILENAME>\n\n', file=sys.stderr)
-
-
 # pylint: disable=too-many-locals
 def main() -> None:
     """do_it()."""
+    arg_vals = None  # Parsed CLI args
     try:
         # LOCAL VARIABLES
-        sig_meta_parser = None            # The SigMFMetaParser (if filepath is a SigMF file)
-        sample_rate = DEF_SAMP_RATE       # Capture sample rate
-        symbol_rate = DEF_SYMB_RATE       # Capture symbol rate
-        sps = 0                           # Samples per symbol
-        filepath = get_filename()         # CLI capture file
-        samples = read_samples(filepath)  # Samples read from the capture
-        translated = samples              # Downshifted samples to 0 Hz center frequency
-        squelch_db = -48                  # Squelch threshold in db
-        spect_analysis = None             # SpectrumAnalysis obj
-        det_signal = None                 # DetectedSignal obj
-        binary = b''                      # Demodulated binary
-        waveform = None                   # Demodulated real waveform
-        index = 0                         # Correlated index
-        new_binary = b''                  # Frame synch'd binary
-        decimate = 20                     # Decimation
+        arg_vals = parse_args()             # Parsed CLI args
+        filepath = get_filename(arg_vals)   # CLI capture file
+        sample_rate = 0                     # Capture sample rate
+        symbol_rate = arg_vals.symbol_rate  # Capture symbol rate
+        sps = 0                             # Samples per symbol
+        samples = None                      # Samples read from the capture
+        decimate = 20                       # Decimation
+        squelch_db = -48                    # Squelch threshold in db (e.g., -48)
+        spect_analysis = None               # SpectrumAnalysis obj
+        det_signal = None                   # DetectedSignal obj
+        metric = None                       # Step 1 - Continuous symbol metric at orig. sample rate
+        symbol_metrics = None               # Step 2 - Recovered symbol metric for each orig. symbol
+        binary = b''                        # Step 3 - Demodulated binary
+        index = 0                           # Correlated index into the binary
+        payload = b''                       # Frame synch'd binary
 
         # PREPARE
-        # [ ] Get Samples
-        # Fetch SigMF metadata (if available)
-        if filepath.suffix.lower() == f'.{SIGMF_DATA_FILE_EXT}'.lower():
-            sig_meta_parser = SigMFMetaParser(filepath.with_suffix(f'.{SIGMF_META_FILE_EXT}'))
-            sample_rate = sig_meta_parser.get_sample_rate()
+        # [!] Determine sample rate
+        sample_rate = get_sample_rate(arg_vals, filepath)
+        # [!] Get Samples
+        samples = read_samples(filepath)
 
-        # [ ] Clean Up
+        # [?] Clean Up
         # Decimate!
-        samples = decimate_samples(samples=samples, decimate=decimate)
-        sample_rate = sample_rate / decimate
-        sps = calculate_sps(sample_rate=sample_rate, symbol_rate=symbol_rate)  # Calc the sps
+        if decimate > 1:
+            samples = decimate_samples(samples=samples, decimate=decimate)
+            sample_rate = sample_rate / decimate  # Update the sample rate
+            sps = calculate_sps(sample_rate=sample_rate, symbol_rate=symbol_rate)  # Calc new sps
 
-        # [ ] Squelch!
-        samples = squelch_signal(samples=samples, threshold=squelch_db)  # -48 is the sweet spot
+        # [?] Squelch!
+        if squelch_db is not None:
+            samples = squelch_signal(samples=samples, threshold=squelch_db)
 
-        # [ ] Analyze the Spectrum
+        # [?] Analyze the Spectrum
         spect_analysis = analyze_spectrum(samples, sample_rate=sample_rate, max_peaks=2)
 
-        # [ ] Detect Signal
+        # [?] Detect Signal
         det_signal = detect_signal(analysis=spect_analysis, scheme=ModScheme.FSK2)
 
-        # [ ] Downconvert
+        # [?] Downconvert
         if det_signal.center_frequency > 0 or det_signal.center_frequency < 0:
-            translated = downconvert_signal(samples=samples, sample_rate=sample_rate,
-                                            center_freq=det_signal.center_frequency)
-        else:
-            translated = samples
+            samples = downconvert_signal(samples=samples, sample_rate=sample_rate,
+                                         center_freq=det_signal.center_frequency)
 
         # DEMOD
-        # [ ] Demod to Real Symbols
-        waveform = demod_to_waveform(samples=translated, sample_rate=sample_rate,
+        # [?] Steps 1 - 3?
+        # binary = demodulate(samples=samples, sample_rate=sample_rate, symbol_rate=symbol_rate)
+        # -or-
+        # [?] Step 1, 2, then 3!
+        if not binary:
+            # Step 1 - Demod to Metrics
+            metric = demod_to_metric(samples=samples, sample_rate=sample_rate,
                                      symbol_rate=symbol_rate)  # Reshaped to symbol boundaries
-        # [ ] Time Sync w/ Interpolation
-        # mm_wave = recover_clock_mm(waveform, sps, interp=None)  # Do not interpolate
-        mm_wave = recover_clock_mm(waveform, sps, interp=16)  # Interpolate for better boundaries
-        # TO DO: DON'T DO NOW... Get back to the 2nd half of FSK2
-        # binary = demod_to_bytes_partial(real_wave=mm_wave, sample_rate=sample_rate,
-        #                                 symbol_rate=symbol_rate)
-        threshold = numpy.median(mm_wave)
-        bits = (mm_wave > threshold).astype(numpy.uint8)
-        binary = b''.join(str(bit).encode() for bit in bits)
+            # Step 2 - Time Sync w/ Interpolation
+            # symbol_metrics = recover_clock_mm(metric, sps, interp=None)  # Do not interpolate
+            symbol_metrics = recover_clock_mm(metric, sps, interp=16)  # Interp for better boundary
+            # Step 3 - Symbol Decisions
+            binary = decide_symbols(symbol_metrics=symbol_metrics, sample_rate=sample_rate,
+                                    symbol_rate=symbol_rate)
 
-        # [ ] Frame Sync
+        # [?] Frame Sync
         index = correlate_it(binary, DEF_SYNCWORD)
-        new_binary = binary[index + len(DEF_SYNCWORD):]
+        payload = binary[index + len(DEF_SYNCWORD):]
 
-        # [ ] Parse Payload
-        parse_payload(new_binary)
+        # [!] Parse Payload
+        parse_payload(payload)
     except Exception as err:
         print(f'Execution failed with: {repr(err)}', file=sys.stderr, flush=True)
-        raise err from err  # DEBUGGING
+        print_help()
+        if arg_vals and arg_vals.debug is True:
+            raise err from err
 # pylint: enable=too-many-locals
 
 

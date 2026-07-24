e6c4617867d2e1b09e25051fdf1fc236c76c1899
hark130 <hark130@yahoo.com>
2026-07-24 06:19:36 -0500

    GAIN-16: Added attribute to BPSKConfig
    Added optional carrier_recovery to BPSKConfig
    Updated validation system
    
    GOOD NEWS: Legacy test cases are passing as well as manual testing.
    BAD NEWS: I haven't added any carrier_recovery-specific test cases
    yet...  (Just trying to deliver functionality soonest)
    
    Changes to be committed:
    	modified:   gallant_input/modem/bpsk_config.py
    
    Changes not staged for commit:
    	modified:   gallant_input/signal.py
    	modified:   rxtx/static_rx_template.py
    	modified:   test/unit_test/test_modem/test_bpsk/test_modem_bpsk.py
    	modified:   test/unit_test/test_modem/test_bpsk/test_modem_bpsk_demodulate.py
    	modified:   test/unit_test/test_modem/test_bpsk/test_modem_bpsk_modulate.py

--- gallant_input/modem/bpsk_config.py
+++ gallant_input/modem/bpsk_config.py
@@ -1,10 +1,12 @@
 """Defines the BPSKConfig data class for use with BPSK()."""
 
 # Standard Imports
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 # Third Party Imports
 # Local Imports
 from gallant_input.modem.modem_config import ModemConfig
+from gallant_input.synch.costas_loop import CostasLoop
+from gallant_input.validation import validate_type
 
 
 @dataclass(kw_only=True)  # Avoid linter false-negatives (e.g., Pylint's unexpected-keyword-arg)
@@ -13,6 +15,8 @@ class BPSKConfig(ModemConfig):
 
     # ATTRIBUTES
     # Public
+
+    carrier_recovery: CostasLoop | None = field(default=None)  # Carrier recovery object
 
     # Private
 
@@ -27,6 +31,22 @@ class BPSKConfig(ModemConfig):
         if self._validated is not True:
             # No additional validation required (yet)
             self.validate_abc()  # Which will complete the validation and set _validated
+            self.validate_bpsk()  # Validate the BPSK-specific data
+
+    def validate_bpsk(self) -> None:
+        """Validate all attributes defined in this child class regardless of internal status."""
+        self._validate_carrier_recovery()
 
     # PUBLIC METHODS
     # In alphabetical order
+
+    # PRIVATE METHODS
+
+    def _validate_carrier_recovery(self) -> None:
+        """Validate the carrier_recover attribute."""
+        if self.carrier_recovery is not None:
+            try:
+                validate_type(self.carrier_recovery, 'carrier_recovery', CostasLoop)
+            except TypeError:
+                raise NotImplementedError('Received an unsupported "carrier recovery" object: '
+                                          f'{type(self.carrier_recovery)}')
